8e9b43186d34d4b7ced7a9779adca210e094a630
Analyzer <analyzer@test.com>
2026-02-09 10:21:00 -0800

    Update

--- code.py
+++ code.py
@@ -1,3 +1,16 @@
-m, x, y = map(int, input().split())
-z = ((x + y - 1) % m) + 1
-print(z)
+import math
+
+n = int(input())
+a = list(map(int, input().split()))
+t = int(input())
+
+for _ in range(t):
+    l, r = map(int, input().split())
+    start = l - 1
+    end = r - 1
+    count = 0
+    for i in range(start, end + 1):
+        for j in range(i + 1, end + 1):
+            if math.gcd(a[i], a[j]) > 1:
+                count += 1
+    print(count)
