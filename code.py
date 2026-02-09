78efdb413b022e7ab96aa82f43fae5ef7b05fa74
Analyzer <analyzer@test.com>
2026-02-09 10:04:41 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,3 @@
-MOD = 10**9 + 7
-
-def main():
-    import sys
-    input = sys.stdin.read
-    data = input().split()
-    n = int(data[0])
-    k = int(data[1])
-    
-    # Precompute factorials and inverse factorials up to 3999
-    max_fact = 3999
-    fact = [1] * (max_fact + 1)
-    for i in range(1, max_fact + 1):
-        fact[i] = fact[i-1] * i % MOD
-    
-    inv_fact = [1] * (max_fact + 1)
-    inv_fact[max_fact] = pow(fact[max_fact], MOD - 2, MOD)
-    for i in range(max_fact - 1, -1, -1):
-        inv_fact[i] = in
+n, m = map(int, input().split())
+result = (n * (n + 1)) % m
+print(result)
