ae21cf53e4f4836417e9046c493bd99f7cc9d88a
Analyzer <analyzer@test.com>
2026-02-09 13:44:11 -0800

    Update

--- code.py
+++ code.py
@@ -1,7 +1,10 @@
-a = int(input())
-b = int(input())
-d = abs(a - b)
-if d % 2 == 0:
-    print(d * (d + 2) // 4)
-else:
-    print((d + 1) ** 2 // 4)
+# Read the number of test cases
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    if N == 1:
+        print(1)
+    else:
+        # Calculate using the formula 6N² - 12N + 8
+        result = 6 * N * N - 12 * N + 8
+        print(result)
