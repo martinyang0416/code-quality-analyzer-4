9b232632a74916cfad942b90831f96fd80a2f981
Analyzer <analyzer@test.com>
2026-02-09 10:37:26 -0800

    Update

--- code.py
+++ code.py
@@ -1,23 +1,20 @@
-def find_min_y0(y2):
-    def get_prime_factors(n):
-        factors = set()
-        i = 2
-        while i * i <= n:
-            while n % i == 0:
-                factors.add(i)
-                n //= i
-            i += 1
-        if n > 1:
-            factors.add(n)
-        return factors
-
-    min_y0 = float('inf')
-
-    p2_list = get_prime_factors(y2)
-
-    for p2 in p2_list:
-        lower = y2
-        upper = y2 + p2
-        for y1 in range(lower, upper):
-            if p2 >= y1:
-                co
+def main():
+    import sys
+    input = sys.stdin.read().split()
+    n = int(input[0])
+    h = list(map(int, input[1:n+1]))
+    
+    next_tree = [ -1 ] * n
+    
+    # Compute next shorter tree for each position using a stack
+    stack = []
+    for i in range(n-1, -1, -1):
+        while stack and h[stack[-1]] >= h[i]:
+            stack.pop()
+        if stack:
+            next_tree[i] = stack[-1]
+        else:
+            next_tree[i] = -1
+        stack.append(i)
+    
+    # Now determine which tree
