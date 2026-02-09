3bc5f565faf6b96bd1b4d73848497589eab1686f
Analyzer <analyzer@test.com>
2026-02-09 13:21:51 -0800

    Update

--- code.py
+++ code.py
@@ -1,9 +1,17 @@
-n, k = map(int, input().split())
-a = list(map(int, input().split()))
-min_hours = float('inf')
-for ai in a:
-    if ai <= k and k % ai == 0:
-        current = k // ai
-        if current < min_hours:
-            min_hours = current
-print(min_hours)
+T = int(input())
+for _ in range(T):
+    N, M = map(int, input().split())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+    i = j = 0
+    merged = []
+    while i < N and j < M:
+        if A[i] >= B[j]:
+            merged.append(A[i])
+            i += 1
+        else:
+            merged.append(B[j])
+            j += 1
+    merged.extend(A[i:])
+    merged.extend(B[j:])
+    print(' '.join(map(str, merged)))
