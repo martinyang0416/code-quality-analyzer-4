534a549292082261245a6131c13fa338e5a6d653
Analyzer <analyzer@test.com>
2026-02-09 15:10:20 -0800

    Update

--- code.py
+++ code.py
@@ -1,23 +1,9 @@
-import sys
-
-def find_max_subarray(arr):
-    max_sum = -1
-    max_length = 0
-    max_start = 0
-    max_end = 0
-    current_sum = 0
-    current_length = 0
-    current_start = -1
-
-    n = len(arr)
-    for i in range(n):
-        num = arr[i]
-        if num >= 0:
-            if current_start == -1:
-                current_start = i
-                current_sum = num
-                current_length = 1
-            else:
-                current_sum += num
-                current_length += 1
-            
+T = int(input())
+for _ in range(T):
+    K = int(input())
+    for i in range(K):
+        row = [' '] * K
+        for j in range(K):
+            if j == 0 or j == K-1 or j == i or j == (K-1 - i):
+                row[j] = '1'
+        print(''.join(row))
