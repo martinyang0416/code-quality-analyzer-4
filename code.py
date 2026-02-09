92b48e6ac73ddfca83e32737e5cc13cc88ad7006
Analyzer <analyzer@test.com>
2026-02-09 13:22:40 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,21 @@
-import sys
+MOD = 10**9 + 7
 
-def main():
-    data = sys.stdin.read().split()
-    t = int(data[0])
-    res = []
-    idx = 1
-    for _ in range(t):
-        n = int(data[idx])
-        k = int(data[idx + 1])
-        idx += 2
-        if (n - 1) % (k + 1) == 0:
-            res.append("Nandu")
-        else:
-            res.append("Rani")
-    print('\n'.join(res))
+n, m = map(int, input().split())
+broken = set()
+for _ in range(m):
+    a = int(input())
+    broken.add(a)
 
-if __name__ == "__main__":
-    main()
+dp = [0] * (n + 1)
+dp[0] = 1  # Starting point
+
+for i in range(1, n + 1):
+    if i in broken:
+        dp[i] = 0
+    else:
+        dp[i] = dp[i-1]  # Add ways from previous step
+        if i >= 2:
+            dp[i] += dp[i-2]  # Add ways from two steps back
+        dp[i] %= MOD  # Apply modulo to prevent overflow
+
+print(dp[n] % MOD)
