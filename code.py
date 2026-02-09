03b6b17790c9546be1e9b4d004e6fb3431102841
Analyzer <analyzer@test.com>
2026-02-09 10:37:53 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,22 @@
-def main():
-    import sys
-    input = sys.stdin.read().split()
-    n = int(input[0])
-    h = list(map(int, input[1:n+1]))
-    
-    next_tree = [ -1 ] * n
-    
-    # Compute next shorter tree for each position using a stack
-    stack = []
-    for i in range(n-1, -1, -1):
-        while stack and h[stack[-1]] >= h[i]:
-            stack.pop()
-        if stack:
-            next_tree[i] = stack[-1]
-        else:
-            next_tree[i] = -1
-        stack.append(i)
-    
-    # Now determine which tree
+import sys
+
+def find_special_number(M):
+    L = (M + 7) // 8
+    U = M // 5
+    if L > U:
+        return "-1"
+    else:
+        for N in range(L, U + 1):
+            temp = M - 5 * N
+            if temp % 3 != 0:
+                continue
+            k = temp // 3
+            if k < 0 or k > N:
+                continue
+            # Form the number
+            s = '5' * (N - k) + '8' * k
+            return s
+        return "-1"
+
+M = int(sys.stdin.readline())
+print(find_special_number(M))
