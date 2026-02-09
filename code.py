1416d93674b01ad035f9a41434bd5daeb13ca03b
Analyzer <analyzer@test.com>
2026-02-09 12:16:56 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,20 @@
-#### Answer to <https://codeforces.com/problemset/problem/251/A>
-# import math
+import math
+from sys import stdin
+from sys import setrecursionlimit
+setrecursionlimit(100000)
+def put():  return  map(int, stdin.readline().split())
+for _ in range(int(input())):
+	n=int(input())
+	if(n%3==0):
+		print(n//3,0,0)
+	elif(n%3==1):
+		if(n//3>=2):
+			print((n//3)-2,0,1)
+		else:
+			print(-1)
+	else:
+		if(n//3>=1):
+			print((n//3)-1,1,0)
+		else:
+			print(-1)
 
-# count, bounds = [int(x) for x in input().split(" ")]
-# total = 0
-
-# left_index = 0
-# right_index = 2
-# current = 0
-
-# lst = [int(x) for x in input().split(" ")]
-# if lst[0] < 0:
-#     delta = 1 - lst[0]
-#     lst = [x + delta for x in lst]
-
-# while right_index < len(lst):
-#     if lst[right_index] - lst[left_index] <= bounds:
-#         right_index += 1
-#         continue
-#     elements = right_index - left_index - 
