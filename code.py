022b34401b0608320579ef5a2b69c40fd99a36f2
Analyzer <analyzer@test.com>
2026-02-09 10:49:08 -0800

    Update

--- code.py
+++ code.py
@@ -1,7 +1,19 @@
 a = int(input())
-perm = [4, 1, 3, 2, 0, 5]
+bits = [0] * 6
+for i in range(6):
+    bits[i] = (a >> i) & 1
+
+new_bits = [
+    bits[3],  # output0
+    bits[1],  # output1
+    bits[4],  # output2
+    bits[2],  # output3
+    bits[0],  # output4
+    bits[5],  # output5
+]
+
 result = 0
 for i in range(6):
-    if a & (1 << i):
-        result += 1 << perm[i]
+    result += new_bits[i] << i
+
 print(result)
