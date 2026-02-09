f78a1b98b5002c9c2ade3c5b8cac6ddeeb23210f
Analyzer <analyzer@test.com>
2026-02-09 10:29:02 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,21 @@
-def sieve(max_sum):
-    if max_sum < 2:
-        return [False] * (max_sum + 1)
-    is_prime = [True] * (max_sum + 1)
-    is_prime[0] = is_prime[1] = False
-    for i in range(2, int(max_sum ** 0.5) + 1):
-        if is_prime[i]:
-            for j in range(i * i, max_sum + 1, i):
-                is_prime[j] = False
-    return is_prime
+import sys
 
 def main():
-    import sys
     input = sys.stdin.read().split()
-    N = int(input[0])
-    arr = list(map(int, input[1:N+1]))
-    
-    total = sum(arr)
-    if total <
+    idx = 0
+    T = int(input[idx])
+    idx += 1
+    for _ in range(T):
+        N = int(input[idx])
+        M = int(input[idx+1])
+        idx += 2
+        minScore = list(map(int, input[idx:idx+N]))
+        idx += N
+        scholarships = []
+        for _ in range(M):
+            r = int(input[idx])
+            ma = int(input[idx+1])
+            scholarships.append((r, ma, 0))
+            idx += 2
+        qual = []
+        for _ in ran
