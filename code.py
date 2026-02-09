b576ffc67aad74b029eb272f773090970a7e662c
Analyzer <analyzer@test.com>
2026-02-09 10:09:50 -0800

    Update

--- code.py
+++ code.py
@@ -1,2 +1,14 @@
 n = int(input())
-print(5 + 3 * n)
+directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # North, East, South, West
+
+while n != -1:
+    steps = 2 * n
+    x, y = 0, 0
+    for j in range(steps):
+        dir_idx = j % 4
+        dx, dy = directions[dir_idx]
+        x += dx
+        y += dy
+    print(x)
+    print(y)
+    n = int(input())
