429a4ae329e752b6ebbdccefa63a87cf00164ff3
Analyzer <analyzer@test.com>
2026-02-09 15:15:40 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,21 @@
-MOD = 10**9 + 7
-max_fact = 4000
+S = input().strip()
+n = len(S)
+max_num = 0
 
-# Precompute factorials and inverse factorials up to max_fact
-fact = [1] * (max_fact + 1)
-for i in range(1, max_fact + 1):
-    fact[i] = fact[i-1] * i % MOD
-
-inv_fact = [1] * (max_fact + 1)
-inv_fact[max_fact] = pow(fact[max_fact], MOD-2, MOD)
-for i in range(max_fact - 1, -1, -1):
-    inv_fact[i] = inv_fact[i + 1] * (i + 1) % MOD
-
-# Read input
-N, K = map(int, input().split())
-total_n = N + K - 1
-
-# Calculate combination
-result = fact[total_n] * inv_fact[K] % MOD
-r
+for a in range(n):
+    current_letters = 0
+    pos = -1
+    for b in range(a, n):
+        if S[b].isalpha():
+            current_letters += 1
+            if current_letters == 1:
+                pos = b
+            else:
+                break  # More than one letter, stop this a's loop
+        if current_letters > 1:
+            break
+        # Generate candidate
+        if current_letters == 0:
+            candidate = S[a:b+1]
+        else:
+          
