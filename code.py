9d17f672ba9d49b0b7994a382d31f26e4e1f5993
Analyzer <analyzer@test.com>
2026-02-09 15:16:54 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,6 @@
-from collections import Counter
-
-def is_dynamic(s):
-    freq = list(Counter(s).values())
-    freq.sort()
-    n = len(freq)
-    if n < 3:
-        return True
-    if n == 3:
-        return freq[0] + freq[1] == freq[2]
-    else:
-        # Check two possible permutations: original and first two swapped
-        def check_sequence(lst):
-            for i in range(2, len(lst)):
-                if lst[i] != lst[i-1] + lst[i-2]:
-                    return False
-            return True
-        
-        # 
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    total = sum(a)
+    print("Yes" if total % n == 0 else "No")
