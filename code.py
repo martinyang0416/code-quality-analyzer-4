70dcf2c5f85ccc2be5629bc44911d38549034c07
Analyzer <analyzer@test.com>
2026-02-09 10:13:18 -0800

    Update

--- code.py
+++ code.py
@@ -1,6 +1,25 @@
-n = int(input())
-scores = list(map(int, input().split()))
-min_score = min(scores)
-third_student_score = scores[2]
-modified_score = (min_score ^ third_student_score) + 5
-print(modified_score)
+MOD = 10**9 + 7
+
+def main():
+    import sys
+    N, K, P = map(int, sys.stdin.readline().split())
+    B = list(map(int, sys.stdin.readline().split()))
+    
+    if N != 2 * K:
+        print(0)
+        return
+    
+    if P > N:
+        print(0)
+        return
+    
+    from collections import defaultdict
+    count_B = defaultdict(int)
+    for num in B:
+        if num < 1 or num > K:
+            print(0)
+            return
+        count_B[num] += 1
+    
+    for num in count_B:
+        if count_B[num]
