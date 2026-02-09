c6c4d3d37f6fb993d70b3e0b7122dd17d005d12a
Analyzer <analyzer@test.com>
2026-02-09 14:10:47 -0800

    Update

--- code.py
+++ code.py
@@ -1,14 +1,22 @@
-y, k, n = map(int, input().split())
+n = int(input())
+cards = list(map(int, input().split()))
+left = 0
+right = n - 1
+sereja = 0
+dima = 0
+turn = True  # True for Sereja's turn
 
-start = (y + 1 + k - 1) // k * k
-max_s = (n // k) * k
+while left <= right:
+    if cards[left] > cards[right]:
+        selected = cards[left]
+        left += 1
+    else:
+        selected = cards[right]
+        right -= 1
+    if turn:
+        sereja += selected
+    else:
+        dima += selected
+    turn = not turn
 
-if start > max_s:
-    print(-1)
-else:
-    res = []
-    current = start
-    while current <= max_s:
-        res.append(current - y)
-        current += k
-    print(' '.join(map(str, res)))
+print(sereja, dima)
