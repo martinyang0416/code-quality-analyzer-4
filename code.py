19d6311537f1e30aadc9c5265d3cf74a79cb4ce3
Analyzer <analyzer@test.com>
2026-02-09 13:28:31 -0800

    Update

--- code.py
+++ code.py
@@ -1,11 +1,16 @@
-import math
+l, r = map(int, input().split())
 
-x, y, z = map(float, input().split())
-
-expressions = [
-    {'index': 1, 'expr': 'x^y^z', 'type': 1, 'A': z * math.log(y), 'B': math.log(x)},
-    {'index': 2, 'expr': 'x^z^y', 'type': 1, 'A': y * math.log(z), 'B': math.log(x)},
-    {'index': 3, 'expr': '(x^y)^z', 'type': 2, 'log_val': y * z * math.log(x)},
-    {'index': 4, 'expr': '(x^z)^y', 'type': 2, 'log_val': z * y * math.log(x)},
-    {'index': 5, 'expr': 'y^x^z', 'type': 1, 'A': z * math.log(x), 'B': math.log(y)},
-    {'index': 
+if l == r:
+    n = l
+    if n % 2 == 0:
+        print(2)
+    else:
+        i = 3
+        while i * i <= n:
+            if n % i == 0:
+                print(i)
+                exit()
+            i += 2
+        print(n)
+else:
+    print(2)
