a332d2a7f97ca107712f124fdd4c13c56bbaff29
Analyzer <analyzer@test.com>
2026-02-09 14:27:44 -0800

    Update

--- code.py
+++ code.py
@@ -1,6 +1,7 @@
-from collections import Counter
-
-def frequency_sort(s):
-    counts = Counter(s)
-    sorted_chars = sorted(counts.keys(), key=lambda c: (-counts[c], c))
-    return ''.join([char * counts[char] for char in sorted_chars])
+def optimalDivision(nums):
+    if len(nums) == 1:
+        return str(nums[0])
+    elif len(nums) == 2:
+        return f"{nums[0]}/{nums[1]}"
+    else:
+        return f"{nums[0]}/({'/'.join(map(str, nums[1:]))})"
