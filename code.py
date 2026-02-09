736991ed50c292b525f6d02207d78895f39b939b
Analyzer <analyzer@test.com>
2026-02-09 13:37:55 -0800

    Update

--- code.py
+++ code.py
@@ -1,12 +1,11 @@
-T = int(input())
-for _ in range(T):
-    s = input().strip()
-    r = s[::-1]
-    funny = True
-    for i in range(1, len(s)):
-        s_diff = abs(ord(s[i]) - ord(s[i-1]))
-        r_diff = abs(ord(r[i]) - ord(r[i-1]))
-        if s_diff != r_diff:
-            funny = False
-            break
-    print("Funny" if funny else "Not Funny")
+s = input().strip()
+target = 'keyence'
+
+for i in range(len(target) + 1):
+    prefix = target[:i]
+    suffix = target[i:]
+    if s.startswith(prefix) and s.endswith(suffix):
+        print("YES")
+        exit()
+
+print("NO")
