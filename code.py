a13e3af0b7235fce8e4ae847e92212dc0a7f600e
Analyzer <analyzer@test.com>
2026-02-09 15:18:14 -0800

    Update

--- code.py
+++ code.py
@@ -1,15 +1,12 @@
-for _ in range(int(input())):
-    n, m = map(int, input().split())
-    grid = [input().strip() for _ in range(n)]
-    s = input().strip()
-    for d in s:
-        if d in 'LR':
-            new_grid = []
-            for row in grid:
-                cnt = row.count('1')
-                if d == 'L':
-                    new_row = '1' * cnt + '0' * (m - cnt)
-                else:
-                    new_row = '0' * (m - cnt) + '1' * cnt
-                new_grid.append(new_row)
-            grid = new_g
+T = int(input())
+for _ in range(T):
+    N, K, V = map(int, input().split())
+    A = list(map(int, input().split()))
+    sum_A = sum(A)
+    required_total = V * (N + K)
+    delta = required_total - sum_A
+    if delta <= 0 or delta % K != 0:
+        print(-1)
+    else:
+        x = delta // K
+        print(x)
