d5960b43c0a3dfb895648d07e5973522783c56f1
Analyzer <analyzer@test.com>
2026-02-09 14:53:41 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,20 @@
-def decodeAtIndex(S, K):
-    stack = []
-    current_length = 0
-    for c in S:
-        if c.isalpha():
-            current_length += 1
-            stack.append(c)
-            if current_length == K:
-                return c
-        else:
-            d = int(c)
-            if current_length * d < K:
-                current_length *= d
-                stack.append(c)
-            else:
-                current_length *= d
-                stack.append(c)
-                break
+class BIT:
+    def __init__(self, size):
+        self.size = size
+        self.tree = [0] * (self.size + 2)  # Use +2 to avoid issues with 1-based indexing
     
-    while stack:
-  
+    def update(self, idx, delta):
+        while idx <= self.size:
+            self.tree[idx] += delta
+            idx += idx & -idx
+    
+    def query(self, idx):
+        res = 0
+        while idx > 0:
+            res += self.tree[idx]
+            idx -= idx & -idx
+        return res
+
+def min_swaps(grid):
+    n = len(grid)
+    max_pos = 
