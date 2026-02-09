eff9b02f886e0bca2bede0850eb43bee3212b567
Analyzer <analyzer@test.com>
2026-02-09 13:55:46 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,10 @@
-n = int(input())
+from collections import Counter
+
 s = input().strip()
-
-result = []
-used = set()
-current_start = 0
-
-while True:
-    max_char = None
-    max_pos = -1
-    # Iterate through the current segment to find the highest available character
-    for i in range(current_start, n):
-        c = s[i]
-        if c not in used:
-            if max_char is None or c > max_char:
-                max_char = c
-                max_pos = i
-    if max_char is None:
-        break  # No more characters can be added
-    result.append(max_char
+counts = Counter(s)
+n = len(s)
+max_count = max(counts.values())
+if max_count > (n + 2) // 3:
+    print("NO")
+else:
+    print("YES")
