13573880278e2930b1ee2650b15df4c3f0a96311
Analyzer <analyzer@test.com>
2026-02-09 14:36:20 -0800

    Update

--- code.py
+++ code.py
@@ -1,15 +1,11 @@
-def longestArithSeqLength(A):
-    n = len(A)
-    if n <= 2:
-        return n
-    dp = [{} for _ in range(n)]
-    max_len = 2
-    for j in range(n):
-        for i in range(j):
-            d = A[j] - A[i]
-            prev = dp[i].get(d, 1)
-            current = prev + 1
-            dp[j][d] = max(current, dp[j].get(d, 0))
-            if dp[j][d] > max_len:
-                max_len = dp[j][d]
-    return max_len
+def minSwap(s1: str, s2: str) -> int:
+    total_x = s1.count('x') + s2.count('x')
+    if total_x % 2 != 0:
+        return -1
+    xy = yx = 0
+    for c1, c2 in zip(s1, s2):
+        if c1 == 'x' and c2 == 'y':
+            xy += 1
+        elif c1 == 'y' and c2 == 'x':
+            yx += 1
+    return (xy // 2) + (yx // 2) + 2 * (xy % 2)
