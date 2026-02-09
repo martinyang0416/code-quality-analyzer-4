8175ef47abf5eecb1a3ada560c2938b68e6a9593
Analyzer <analyzer@test.com>
2026-02-09 10:34:03 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,19 @@
-import sys
-
 def main():
-    input = sys.stdin.read().split()
-    idx = 0
-    t = int(input[idx])
-    idx += 1
-    for _ in range(t):
-        m = int(input[idx])
-        y = int(input[idx+1])
-        idx += 2
-        b = list(map(int, input[idx:idx+m]))
-        idx += m
-        
-        even_count = 0
-        for num in b:
-            if num % 2 == 0:
-                even_count += 1
-        odd_count = m - even_count
-        
-        # Calculate the minimum and maximum possible number of odds (o)
+    import sys
+    N, K, T, U = map(int, sys.stdin.readline().split())
+    H = list(map(int, sys.stdin.readline().split()))
+    
+    cut_list = []
+    for h in H:
+        if h < T:
+            cut_list.append(h)
+    
+    max_allowed = N - K
+    if len(cut_list) <= max_allowed:
+        print(len(cut_list))
+        return
+    
+    # Re-evaluation process
+    # Start from the end of the cut_list and check in reverse
+    i = len(cut_list) - 1
+    while i >= 0 and len(cut_list) > max_allo
