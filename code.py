5fcbc960140c0dbb3ec0c52cc92f1933f4ec90ef
Analyzer <analyzer@test.com>
2026-02-09 15:03:14 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,12 @@
-import sys
-from collections import deque
+# Read the two numbers from input
+n1 = int(input())
+n2 = int(input())
 
-def main():
-    N, M = map(int, sys.stdin.readline().split())
-    adj = [[] for _ in range(N+1)]
-    for _ in range(M):
-        u, v, c = map(int, sys.stdin.readline().split())
-        adj[u].append((v, c))
-        adj[v].append((u, c))
-    
-    # BFS to find spanning tree
-    parent = [0] * (N + 1)
-    edge_c = [0] * (N + 1)
-    visited = [False] * (N + 1)
-    q = deque()
-    q.append(1)
-    visited[1] = True
-    parent[1] = 0
-    edge_c[1] = 0  # dummy
+# Check if the first number is greater than the second
+if n1 > n2:
+    result = n1 - n2
+else:
+    result = n1 + n2
+
+# Output the result
+print(result)
