2c5eadf3e6024cc6307a6a98e0cdbd0ee53987d8
Analyzer <analyzer@test.com>
2026-02-09 14:18:38 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,15 @@
-import bisect
+from functools import lru_cache
 
-def findLengthOfShortestSubarray(arr):
-    n = len(arr)
-    if n <= 1:
-        return 0
-    
-    # Find the end of the longest non-decreasing prefix
-    left_end = 0
-    for i in range(1, n):
-        if arr[i] >= arr[i-1]:
-            left_end = i
-        else:
-            break
-    if left_end == n - 1:
-        return 0
-    
-    # Find the start of the longest non-decreasing suffix
-    right_start = n - 1
-    for i in range(n-2, -1, -1):
-        if arr[i] <= arr[i+1]:
-           
+class Solution:
+    def isScramble(self, s1: str, s2: str) -> bool:
+        @lru_cache(maxsize=None)
+        def scramble(a, b):
+            if a == b:
+                return True
+            if sorted(a) != sorted(b):
+                return False
+            n = len(a)
+            for k in range(1, n):
+                if (scramble(a[:k], b[:k]) and scramble(a[k:], b[k:])) or \
+                   (scramble(a[:k], b[-k:]) and scramble(a[k:], b[:-k])):
+            
