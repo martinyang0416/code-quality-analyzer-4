4b9175e5280a30d98014cf695a48116707c679cf
Analyzer <analyzer@test.com>
2026-02-09 10:27:18 -0800

    Update

--- code.py
+++ code.py
@@ -1,23 +1,21 @@
 import sys
 
-def minimal_changes(s):
-    if not s:
-        return 0
-    total = 0
-    current_char = s[0]
-    run_length = 1
-    for c in s[1:]:
-        if c == current_char:
-            run_length += 1
-        else:
-            total += (run_length - 1) // 2
-            current_char = c
-            run_length = 1
-    total += (run_length - 1) // 2
-    return total
+MOD = 10**9 + 7
 
 def main():
-    t = int(sys.stdin.readline())
-    for _ in range(t):
-        s = sys.stdin.readline().strip()
-        print(minimal_
+    Q = int(sys.stdin.readline())
+    for _ in range(Q):
+        M = int(sys.stdin.readline())
+        B = list(map(int, sys.stdin.readline().split()))
+        
+        existing_dests = set()
+        dest_freq = {}
+        invalid = False
+        
+        for idx in range(M):
+            val = B[idx]
+            if val != 0:
+                player = idx + 1
+                if val == player:
+                    invalid = True
+                if val < 1 or 
