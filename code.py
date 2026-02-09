4a97e5b69cb319636129c1dd6107f80873da44e8
Analyzer <analyzer@test.com>
2026-02-09 14:30:16 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,17 @@
-from itertools import permutations
-
-def isSolvable(words, result):
-    max_word_length = max(len(word) for word in words)
-    result_length = len(result)
-    
-    if result_length not in [max_word_length, max_word_length + 1]:
-        return False
-    
-    leading_chars = set()
-    for word in words:
-        leading_chars.add(word[0])
-    leading_chars.add(result[0])
-    
-    chars = set()
-    for word in words:
-        chars.update(word)
-    chars.update(result)
-    chars = list(chars)
-    
-   
+def evalRPN(tokens):
+    stack = []
+    for token in tokens:
+        if token in '+-*/':
+            b = stack.pop()
+            a = stack.pop()
+            if token == '+':
+                stack.append(a + b)
+            elif token == '-':
+                stack.append(a - b)
+            elif token == '*':
+                stack.append(a * b)
+            elif token == '/':
+                stack.append(int(a / b))
+        else:
+            stack.append(int(token))
+    return stack.pop()
