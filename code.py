4f490849ecfecd3da86d5a1ff63cbfaa2fffd570
Analyzer <analyzer@test.com>
2026-02-09 10:04:16 -0800

    Update

--- code.py
+++ code.py
@@ -1,14 +1,19 @@
-n, b = map(int, input().split())
-v = int(input())
-max_h = 0.0
+MOD = 10**9 + 7
 
-for _ in range(n):
-    x, h = map(int, input().split())
-    if v < x < b:
-        numerator = h * (b - v)
-        denominator = x - v
-        H = numerator / denominator
-        if H > max_h:
-            max_h = H
-
-print("{0:.6f}".format(max_h))
+def main():
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+    n = int(data[0])
+    k = int(data[1])
+    
+    # Precompute factorials and inverse factorials up to 3999
+    max_fact = 3999
+    fact = [1] * (max_fact + 1)
+    for i in range(1, max_fact + 1):
+        fact[i] = fact[i-1] * i % MOD
+    
+    inv_fact = [1] * (max_fact + 1)
+    inv_fact[max_fact] = pow(fact[max_fact], MOD - 2, MOD)
+    for i in range(max_fact - 1, -1, -1):
+        inv_fact[i] = in
