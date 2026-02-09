15eadce7a596af98417656ed26e3cfa94c5cc199
Analyzer <analyzer@test.com>
2026-02-09 10:28:05 -0800

    Update

--- code.py
+++ code.py
@@ -1,16 +1,6 @@
-t = int(input())
-for _ in range(t):
-    n = input().strip()
-    summands = []
-    length = len(n)
-    for i in range(length):
-        d = int(n[i])
-        if d != 0:
-            power = 10 ** (length - 1 - i)
-            summand = d * power
-            summands.append(summand)
-    print(len(summands))
-    if summands:
-        print(' '.join(map(str, summands)))
-    else:
-        print()
+M = int(input())
+B = list(map(int, input().split()))
+if len(B) == len(set(B)):
+    print("UNIQUE")
+else:
+    print("DUPLICATE")
