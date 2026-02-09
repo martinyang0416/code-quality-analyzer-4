3511e031d26637cf7bc6abe38ddb003a58051ff2
Analyzer <analyzer@test.com>
2026-02-09 13:43:15 -0800

    Update

--- code.py
+++ code.py
@@ -1,7 +1,20 @@
-n, m = map(int, input().split())
-a = list(map(int, input().split()))
-from collections import Counter
-counts = Counter(a)
-total = n * (n - 1) // 2
-same = sum(c * (c - 1) // 2 for c in counts.values())
-print(total - same)
+import sys
+
+class SegmentTreeNode:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.left = None
+        self.right = None
+        self.sorted_li = []
+        self.prefix_max = []
+
+def build_tree(ropes, start, end):
+    node = SegmentTreeNode(start, end)
+    if start == end:
+        li, ri = ropes[start]
+        node.sorted_li = [(li, ri)]
+        node.prefix_max = [ri]
+    else:
+        mid = (start + end) // 2
+        node.left = build_tree(rope
