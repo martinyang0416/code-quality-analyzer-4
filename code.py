308173959b80ab24fb9885f3ec7e7761c64c3dc9
Analyzer <analyzer@test.com>
2026-02-09 15:04:30 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,22 @@
-import math
+import sys
 
-def main():
-    T = int(input())
-    for _ in range(T):
-        n = int(input())
-        x = list(map(int, input().split()))
-        P, Q = map(int, input().split())
-        x.sort()
-        total = 0.0
-        for i in range(n // 2):
-            a = x[i]
-            b = x[n - 1 - i]
-            dx1 = a - P
-            dx2 = b - P
-            dot_product = dx1 * dx2 + Q * Q
-            mag1 = math.hypot(dx1, Q)
-            mag2 = math.hypot(dx2, Q)
-            if mag1 == 0 or mag2 =
+def calculate_difference(s):
+    if not s:
+        return 0
+    groups = []
+    current_char = s[0]
+    count = 1
+    for c in s[1:]:
+        if c == current_char:
+            count += 1
+        else:
+            groups.append((current_char, count))
+            current_char = c
+            count = 1
+    groups.append((current_char, count))
+    
+    compressed_size = 0
+    for char, cnt in groups:
+        compressed_size += 8
+        if cnt >= 2:
+            compressed_size += 32 * le
