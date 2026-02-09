0d19b125b37160aa073ded6019f31bd1cb5547b3
Analyzer <analyzer@test.com>
2026-02-09 13:45:17 -0800

    Update

--- code.py
+++ code.py
@@ -1,12 +1,20 @@
-t = int(input())
-for _ in range(t):
-    n = int(input())
-    matrix = [input().strip() for _ in range(n)]
-    star_row, star_col = 0, 0
-    for i in range(n):
-        if '*' in matrix[i]:
-            star_row = i
-            star_col = matrix[i].index('*')
-            break
-    m = n // 2
-    print(abs(star_row - m) + abs(star_col - m))
+import sys
+
+n = int(sys.stdin.readline())
+a = [int(sys.stdin.readline()) for _ in range(n)]
+max_val = max(a)
+max_count = a.count(max_val)
+second_max = -float('inf')
+
+for num in a:
+    if num < max_val and num > second_max:
+        second_max = num
+
+for num in a:
+    if num < max_val:
+        print(max_val)
+    else:
+        if max_count > 1:
+            print(max_val)
+        else:
+            print(second_max if second_max != -float('inf') else max_val)
