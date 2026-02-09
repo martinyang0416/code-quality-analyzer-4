c29f1c04343be96d0c6b1970e2f638b1f9f3c769
Analyzer <analyzer@test.com>
2026-02-09 14:21:27 -0800

    Update

--- code.py
+++ code.py
@@ -1,16 +1,11 @@
-from collections import Counter
-
-def minSetSize(arr):
-    n = len(arr)
-    target = n // 2
-    freq = Counter(arr)
-    sorted_freq = sorted(freq.values(), reverse=True)
-    
-    count = 0
-    total = 0
-    for f in sorted_freq:
-        total += f
-        count += 1
-        if total >= target:
-            break
-    return count
+class Solution:
+    def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
+        costs = [abs(ord(sc) - ord(tc)) for sc, tc in zip(s, t)]
+        left = current_sum = max_len = 0
+        for right in range(len(costs)):
+            current_sum += costs[right]
+            while current_sum > maxCost:
+                current_sum -= costs[left]
+                left += 1
+            max_len = max(max_len, right - left + 1)
+        return max_len
