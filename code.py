a85e3f7d3b68f481103f9fe94d365967593ae17e
Analyzer <analyzer@test.com>
2026-02-09 10:51:03 -0800

    Update

--- code.py
+++ code.py
@@ -1,5 +1,34 @@
-Okay, I need to solve this programming problem where the input is an integer a between 1 and 30, and the output is another integer. The example given is input 3 outputs 27. Let me think about what's going on here. 
-
-Hmm, 3 cubed is 27. Oh right! So maybe the problem is to compute a cubed? Let me check. If the input is 3, then 3^3 is 27. That fits. Let me see if there's any other possibility. Like maybe a factorial? But 3 factorial is 6, which doesn't match. So cubing seems likely. 
-
-So the task 
+a = int(input())
+presidents = [
+    "Washington",
+    "Adams",
+    "Jefferson",
+    "Madison",
+    "Monroe",
+    "Adams",
+    "Jackson",
+    "Van Buren",
+    "Harrison",
+    "Tyler",
+    "Polk",
+    "Taylor",
+    "Fillmore",
+    "Pierce",
+    "Buchanan",
+    "Lincoln",
+    "Johnson",
+    "Grant",
+    "Hayes",
+    "Garfield",
+    "Arthur",
+    "Cleveland",
+    "Harrison",
+    "Cleveland",
+    "McKinley",
+    "Roosevelt",
+    "Taft",
+    "Wilson",
+    "Harding",
+    "Coolidge",
+    "Hoover",
+    "
