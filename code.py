9ed6cd25cb339b683c57015f1487a2c3981b949a
Analyzer <analyzer@test.com>
2026-02-09 14:28:23 -0800

    Update

--- code.py
+++ code.py
@@ -1,7 +1,19 @@
-def optimalDivision(nums):
-    if len(nums) == 1:
-        return str(nums[0])
-    elif len(nums) == 2:
-        return f"{nums[0]}/{nums[1]}"
-    else:
-        return f"{nums[0]}/({'/'.join(map(str, nums[1:]))})"
+def clumsy(N: int) -> int:
+    if N == 0:
+        return 0
+    current_term = N
+    terms = []
+    current_sign = 1
+    op_index = 0
+    ops = ['*', '/', '+', '-']
+    for i in range(N-1, 0, -1):
+        op = ops[op_index % 4]
+        if op in ('*', '/'):
+            if op == '*':
+                current_term *= i
+            else:
+                current_term = current_term // i
+            op_index += 1
+        else:
+            terms.append(current_sign * current_term)
+            current_sig
