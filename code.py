59741fb3fecf7f5fdfe695f38cb9c8dfa96fdc16
Analyzer <analyzer@test.com>
2026-02-09 13:29:03 -0800

    Update

--- code.py
+++ code.py
@@ -1,16 +1,20 @@
-l, r = map(int, input().split())
+import sys
+from sys import stdin
+from collections import deque
 
-if l == r:
-    n = l
-    if n % 2 == 0:
-        print(2)
-    else:
-        i = 3
-        while i * i <= n:
-            if n % i == 0:
-                print(i)
-                exit()
-            i += 2
-        print(n)
-else:
-    print(2)
+def main():
+    n, m, h = map(int, stdin.readline().split())
+    u = list(map(int, stdin.readline().split()))
+    adj = [[] for _ in range(n)]
+    for _ in range(m):
+        a, b = map(int, stdin.readline().split())
+        a -= 1
+        b -= 1
+        delta = (u[b] - u[a]) % h
+        if delta == 1:
+            adj[a].append(b)
+        elif delta == h - 1:
+            adj[b].append(a)
+    
+    # Kosaraju's algorithm to find SCCs
+ 
