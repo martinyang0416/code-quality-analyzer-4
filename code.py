54845a5299718ea44ac610b4f4dc6a2538c3a607
Analyzer <analyzer@test.com>
2026-02-09 13:42:10 -0800

    Update

--- code.py
+++ code.py
@@ -1,23 +1,17 @@
-def is_sorted(arr, inc):
-    for i in range(len(arr)-1):
-        if inc:
-            if arr[i] > arr[i+1]:
-                return False
-        else:
-            if arr[i] < arr[i+1]:
-                return False
-    return True
+def get_factors(n):
+    counts = {2:0, 3:0, 5:0}
+    for prime in [2, 3, 5]:
+        while n % prime == 0:
+            counts[prime] += 1
+            n = n // prime
+    return counts[2], counts[3], counts[5], n
 
-n = int(input())
-a = list(map(int, input().split()))
+a, b = map(int, input().split())
 
-sorted_inc = is_sorted(a, True)
-sorted_dec = is_sorted(a, False)
+a2, a3, a5, a_rest = get_factors(a)
+b2, b3, b5, b_rest = get_factors(b)
 
-if sorted_inc or sorted_dec:
-    all_same = True
-    for num in a[1:]:
-        if num != a[0]:
-            all_same = False
-            break
-    if a
+if a_rest != b_rest:
+    print(-1)
+else:
+    print(abs(a2 - b2) + abs(a3 - b3) + abs(a5 - b5))
