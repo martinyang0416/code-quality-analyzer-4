d14780f4f229c897b923642d688b825a9dd08c61
Analyzer <analyzer@test.com>
2026-02-09 11:19:22 -0800

    Update

--- code.py
+++ code.py
@@ -1,13 +1,3 @@
 s = input().strip()
-even_sum = 0
-odd_sum = 0
-for c in s:
-    digit = int(c)
-    if digit % 2 == 0:
-        even_sum += digit
-    else:
-        odd_sum += digit
-if even_sum % 2 == 0 and odd_sum % 2 == 1:
-    print("Yes")
-else:
-    print("No")
+total = sum(int(c) for c in s)
+print("Yes" if total % 2 == 1 else "No")
