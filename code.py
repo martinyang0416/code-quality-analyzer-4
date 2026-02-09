4b7a7afa5f5a04122237d93d4f17444e004f4a9e
Analyzer <analyzer@test.com>
2026-02-09 15:09:36 -0800

    Update

--- code.py
+++ code.py
@@ -1,10 +1,23 @@
-# Read the number of test cases
-t = int(input())
-for _ in range(t):
-    a = input().strip()
-    b = input().strip()
-    # Check if there's any common character
-    if set(a) & set(b):
-        print("Yes")
-    else:
-        print("No")
+import sys
+
+def find_max_subarray(arr):
+    max_sum = -1
+    max_length = 0
+    max_start = 0
+    max_end = 0
+    current_sum = 0
+    current_length = 0
+    current_start = -1
+
+    n = len(arr)
+    for i in range(n):
+        num = arr[i]
+        if num >= 0:
+            if current_start == -1:
+                current_start = i
+                current_sum = num
+                current_length = 1
+            else:
+                current_sum += num
+                current_length += 1
+            
