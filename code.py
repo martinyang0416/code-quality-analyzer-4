608f0f77154edea931a90ac3ad3ff682a74d9816
Analyzer <analyzer@test.com>
2026-02-09 12:00:39 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,13 @@
-import sys
-import time
-import itertools
-from itertools import accumulate, product, permutations, combinations
-import collections
-from collections import Counter, OrderedDict, deque, defaultdict, ChainMap
-from functools import lru_cache
-import math
-from math import sqrt, sin, cos, tan, ceil, fabs, floor, gcd, exp, log, log2
-import fractions
-from typing import List, Tuple
-import numpy as np
-import random
-import heapq
-from heapq import *
-from dataclasses import dataclass
-
-import builtins
-import re
+check=[2]
+i=0
+while check[-1]<=10**9:
+    check.append(check[-1]+3*i+5)
+    i+=1
+from bisect import bisect_right
+def dfs(x):
+    if x<2:
+        return 0
+    return dfs(x-check[bisect_right(check,x)-1])+1
+for _ in range(int(input())):
+    n=int(input())
+    print(dfs(n))
