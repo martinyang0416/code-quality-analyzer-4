28fd2ddad2c9c3b31120847f53ea681f14bd9dc3
Analyzer <analyzer@test.com>
2026-02-09 11:15:42 -0800

    Update

--- code.py
+++ code.py
@@ -1,14 +1,10 @@
-a1, a2, a3 = map(int, input().split())
-n = a3 + 1
-
+a1, a2, n = map(int, input().split())
 if n == 1:
-    print(a1)
-elif n == 2:
     print(a2)
 else:
     prev_prev = a1
     prev = a2
-    for _ in range(3, n + 1):
+    for _ in range(2, n + 1):
         current = prev_prev + prev
         prev_prev, prev = prev, current
     print(current)
