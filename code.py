9480e8fdbb238642246d6062d3b9061bb23ba5b7
Analyzer <analyzer@test.com>
2026-02-09 11:16:10 -0800

    Update

--- code.py
+++ code.py
@@ -1,10 +1,19 @@
-a1, a2, n = map(int, input().split())
-if n == 1:
-    print(a2)
-else:
-    prev_prev = a1
-    prev = a2
-    for _ in range(2, n + 1):
-        current = prev_prev + prev
-        prev_prev, prev = prev, current
-    print(current)
+a, b, n = map(int, input().split())
+
+def fib(m):
+    if m == 0:
+        return 0
+    elif m == 1:
+        return 1
+    a, b = 0, 1
+    for _ in range(2, m + 1):
+        c = a + b
+        a = b
+        b = c
+    return b
+
+f_n_minus_1 = fib(n - 1)
+f_n = fib(n)
+
+result = a * f_n_minus_1 + b * f_n
+print(result)
