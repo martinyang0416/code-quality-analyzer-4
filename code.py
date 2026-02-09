2c92824fde28fb1deb5e2e388d8e3c8ea8014607
Analyzer <analyzer@test.com>
2026-02-09 13:48:47 -0800

    Update

--- code.py
+++ code.py
@@ -1,5 +1,13 @@
-from datetime import date
+n = int(input())
+c = list(map(int, input().split()))
+t = list(map(int, input().split()))
 
-day, month, year = map(int, input().split())
-d = date(year, month, day)
-print(d.strftime("%A"))
+if c[0] != t[0] or c[-1] != t[-1]:
+    print("No")
+else:
+    c_diff = [c[i] - c[i-1] for i in range(1, n)]
+    t_diff = [t[i] - t[i-1] for i in range(1, n)]
+    if sorted(c_diff) == sorted(t_diff):
+        print("Yes")
+    else:
+        print("No")
