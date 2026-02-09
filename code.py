a19d0ad2de877f6e14aafd0690696c82cc6a4509
Analyzer <analyzer@test.com>
2026-02-09 14:43:12 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,20 @@
-from math import gcd
-from functools import lru_cache
+import collections
 
-def tilingRectangle(n: int, m: int) -> int:
-    g = gcd(n, m)
-    a, b = n // g, m // g
-    if a > b:
-        a, b = b, a
-
-    @lru_cache(maxsize=None)
-    def dfs(a, b):
-        if a == b:
-            return 1
-        if a == 0 or b == 0:
-            return 0
-        if a > b:
-            a, b = b, a
-        if b % a == 0:
-            return b // a
-        res = a * b  # Upper bound with 1x1 squares
-        for s in range(1, a + 1):
-        
+def longestSubstring(s, k):
+    if len(s) < k:
+        return 0
+    count = collections.Counter(s)
+    invalid_chars = [char for char in count if count[char] < k]
+    if not invalid_chars:
+        return len(s)
+    splits = []
+    start = 0
+    for i, c in enumerate(s):
+        if c in invalid_chars:
+            if start < i:
+                splits.append(s[start:i])
+            start = i + 1
+    if start < len(s):
+        splits.append(s[start:])
+    max_len = 0
+    for spli
