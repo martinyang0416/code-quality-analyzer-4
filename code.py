c27ab26c4d1b95a5b47b66a877ebe53ec3b65294
Analyzer <analyzer@test.com>
2026-02-09 13:34:53 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,26 @@
-mod = 10**9 + 9
+MOD = 10**9 + 7
 
-n, w, b = map(int, input().split())
+n = int(input())
+a = list(map(int, input().split()))
 
-max_fact = 8000  # Sufficiently large to cover all possible combinations
+max_mask = 1 << 20
+cnt = [0] * max_mask
 
-# Precompute factorial and inverse factorial modulo 10^9+9
-fact = [1] * (max_fact + 1)
-for i in range(1, max_fact + 1):
-    fact[i] = fact[i-1] * i % mod
+for num in a:
+    cnt[num] += 1
 
-inv_fact = [1] * (max_fact + 1)
-inv_fact[max_fact] = pow(fact[max_fact], mod-2, mod)
-for i in range(max_fact - 1, -1, -1):
-    inv_fact[i] = inv_fact[i+1] * (i+1) % mod
+for bit in range(20):
+    for mask in range(max_mask):
+        if not (mask & (1 << bit)):
+            cnt[mask] += cnt[mask | (1 << bit)]
 
-def comb(n, k):
-    if k < 0 or k > n:
-        
+sum_terms = 0
+for mask in range(1, max_mask):
+    bits = bin(mask).count('1')
+    f_t = cnt[mask]
+    if f_t == 0:
+        continue
+    term = pow(2, f_t, MOD) - 1
+    if bits % 2 == 1:
+        term = -term
+    sum_
