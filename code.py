1da38d3f81344d5cdb91002cef73657461562c9f
Analyzer <analyzer@test.com>
2026-02-09 10:09:17 -0800

    Update

--- code.py
+++ code.py
@@ -1,13 +1,2 @@
-q = int(input())
-for _ in range(q):
-    h, w = map(int, input().split())
-    rows = []
-    for _ in range(h):
-        row = list(map(int, input().split()))
-        rows.append(row)
-    is_mirrored = True
-    for row in rows:
-        if row != row[::-1]:
-            is_mirrored = False
-            break
-    print("YES" if is_mirrored else "NO")
+n = int(input())
+print(5 + 3 * n)
