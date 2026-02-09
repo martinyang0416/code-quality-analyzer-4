13b9db919a41c552b1cc68b81518fe450e432f80
Analyzer <analyzer@test.com>
2026-02-09 14:19:18 -0800

    Update

--- code.py
+++ code.py
@@ -1,15 +1,13 @@
-from functools import lru_cache
-
-class Solution:
-    def isScramble(self, s1: str, s2: str) -> bool:
-        @lru_cache(maxsize=None)
-        def scramble(a, b):
-            if a == b:
-                return True
-            if sorted(a) != sorted(b):
-                return False
-            n = len(a)
-            for k in range(1, n):
-                if (scramble(a[:k], b[:k]) and scramble(a[k:], b[k:])) or \
-                   (scramble(a[:k], b[-k:]) and scramble(a[k:], b[:-k])):
-            
+def maxDotProduct(nums1, nums2):
+    m, n = len(nums1), len(nums2)
+    dp = [[-float('inf')] * (n + 1) for _ in range(m + 1)]
+    dp[0][0] = 0  # Base case, but not a valid subsequence
+    
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            current = nums1[i-1] * nums2[j-1]
+            # Extend previous subsequences or start new
+            option1 = dp[i-1][j-1] + current
+            # Just take current pair as a new subsequence
+            option2 = current
+           
