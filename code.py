736368488da162f4fc5f038be4f926d3f9c2fa74
Analyzer <analyzer@test.com>
2026-02-09 13:24:23 -0800

    Update

--- code.py
+++ code.py
@@ -1,15 +1,23 @@
-from collections import defaultdict
+import sys
 
-n, k = map(int, input().split())
-arr = list(map(int, input().split()))
-
-freq = defaultdict(int)
-count = 0
-
-for num in arr:
-    r = num % k
-    complement = (k - r) % k
-    count += freq[complement]
-    freq[r] += 1
-
-print(count)
+def main():
+    input = sys.stdin.read().split()
+    ptr = 0
+    T = int(input[ptr])
+    ptr += 1
+    for _ in range(T):
+        N = int(input[ptr])
+        K = int(input[ptr+1])
+        ptr +=2
+        A = list(map(int, input[ptr:ptr+K]))
+        ptr += K
+        m = max(A)
+        if N >= m:
+            print(0)
+        else:
+            res = 1
+            for i in range(1, N+1):
+                res = (res * i) % m
+            print(res % m)
+            
+if __name__ == "__main__":
