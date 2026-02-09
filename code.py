fad425dcbc3698e77dc42058b8e4ac79be05ea3f
Analyzer <analyzer@test.com>
2026-02-09 12:52:55 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,21 @@
-import sys
-import time
-import itertools
-from itertools import accumulate, product, permutations, combinations
-import collections
-from collections import Counter, OrderedDict, deque, defaultdict, ChainMap
-from functools import lru_cache
-import math
-from math import sqrt, sin, cos, tan, ceil, fabs, floor, gcd, exp, log, log2
-import fractions
-from typing import List, Tuple
-import numpy as np
-import random
-import heapq
-from heapq import *
-from dataclasses import dataclass
+class House:
+    c = 0
 
-import builtins
-import re
+    def __init__(self, val):
+        self.id = House.c + 1
+        self.val = int(val)
+        House.c += 1
+
+n = int(input())
+a = list(map(House, input().split()))
+a.sort(key=lambda x: x.val)
+length = 0
+pos1 = pos2 = 1
+for i in range(n * 2):
+    if i % 2:
+        length += abs(pos2 - a[i].id)
+        pos2 = a[i].id
+    else:
+        length += abs(pos1 - a[i].id)
+        pos1 = a[i].id
+print(length)
