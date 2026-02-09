89b072d817d575dcef8a24ed87cb018f1501721d
Analyzer <analyzer@test.com>
2026-02-09 15:05:12 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,20 @@
 import sys
 
-def calculate_difference(s):
-    if not s:
-        return 0
-    groups = []
-    current_char = s[0]
-    count = 1
-    for c in s[1:]:
-        if c == current_char:
-            count += 1
-        else:
-            groups.append((current_char, count))
-            current_char = c
-            count = 1
-    groups.append((current_char, count))
-    
-    compressed_size = 0
-    for char, cnt in groups:
-        compressed_size += 8
-        if cnt >= 2:
-            compressed_size += 32 * le
+def main():
+    t = int(sys.stdin.readline())
+    for _ in range(t):
+        k, n = map(int, sys.stdin.readline().split())
+        arr = list(map(int, sys.stdin.readline().split()))
+        first = -1
+        for i in range(n):
+            if arr[i] == k:
+                first = i + 1
+                break
+        last = -1
+        for i in range(n-1, -1, -1):
+            if arr[i] == k:
+                last = i + 1
+                break
+        if first == -1:
+            print(0)
+ 
