6e2428055c3ca2a468e05b7b8026b107a777a8d1
Analyzer <analyzer@test.com>
2026-02-09 13:32:54 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,19 @@
 import sys
-from collections import deque
 
 def main():
-    n, m, k = map(int, sys.stdin.readline().split())
-    special = list(map(int, sys.stdin.readline().split()))
-    special_set = set(special)
-    edges = []
-    for _ in range(m):
-        u, v, w = map(int, sys.stdin.readline().split())
-        if u != v:
-            edges.append((w, u, v))
-    edges.sort()
+    T = int(sys.stdin.readline())
+    for _ in range(T):
+        a, b, n = map(int, sys.stdin.readline().split())
+        if n == 0:
+            print(a)
+        else:
+            rem = n % 3
+            if rem == 0:
+                print(a)
+            elif rem == 1:
+                print(b)
+            else:
+                print(a ^ b)
 
-    parent = list(range(n+1))
-    def find(u):
-        while parent[u] != u:
-            parent[u] = parent[parent[u]]
-            u = 
+if __name__ == "__main__":
+    main()
