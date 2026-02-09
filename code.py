10efec7876e23400c9b9c415f7005cf7d42b8acd
Analyzer <analyzer@test.com>
2026-02-09 15:11:03 -0800

    Update

--- code.py
+++ code.py
@@ -1,9 +1,25 @@
-T = int(input())
-for _ in range(T):
-    K = int(input())
-    for i in range(K):
-        row = [' '] * K
-        for j in range(K):
-            if j == 0 or j == K-1 or j == i or j == (K-1 - i):
-                row[j] = '1'
-        print(''.join(row))
+import sys
+import math
+
+def get_gcd(arr):
+    from math import gcd
+    current_gcd = arr[0]
+    for num in arr[1:]:
+        current_gcd = gcd(current_gcd, num)
+        if current_gcd == 1:
+            break
+    return current_gcd
+
+def get_divisors(g):
+    if g == 0:
+        return []
+    factors = {}
+    i = 2
+    while i * i <= g:
+        while g % i == 0:
+            factors[i] = factors.get(i, 0) + 1
+            g = g // i
+        i += 1
+    if g > 1:
+        factors[g] = 1
+    divisors = [1]
