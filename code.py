4b9c68d556ef823c68c32dcd901f8da71f8429a2
Analyzer <analyzer@test.com>
2026-02-09 12:54:21 -0800

    Update

--- code.py
+++ code.py
@@ -1,31 +1,19 @@
-def nod(a,b):
-    while a*b!=0:
-        if a>b:
-            a%=b
-        else:
-            b%=a
-    return a+b
-n,m=map(int,input().split())
-l=[int(j) for j in input().split()]
-p=[int(j) for j in input().split()]
+import sys
+import time
+import itertools
+from itertools import accumulate, product, permutations, combinations
+import collections
+from collections import Counter, OrderedDict, deque, defaultdict, ChainMap
+from functools import lru_cache
+import math
+from math import sqrt, sin, cos, tan, ceil, fabs, floor, gcd, exp, log, log2
+import fractions
+from typing import List, Tuple
+import numpy as np
+import random
+import heapq
+from heapq import *
+from dataclasses import dataclass
 
-nd=l[1]-l[0]
-ans=-1
-for i in range(2,n):
-
-    nd=nod(nd,l[i]-l[i-1])
-    
-for i in range(m):
-   
-    if nd%p[i]==0:
-        ans=i
-        break
-    
-if ans==-1:
-    print('NO')
-else:
-    print('YES')
-    print(l[0],ans+1)
-    
-
-    
+import builtins
+import re
