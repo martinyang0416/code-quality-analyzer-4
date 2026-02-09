4013fbc71b506ed889ab34905ad468cfa255923d
Analyzer <analyzer@test.com>
2026-02-09 10:07:12 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,10 @@
-def find_min_k(s):
-    if s == 0:
-        return 0
-    current_fact = 1
-    current_sum = 1
-    k = 1
-    if current_sum >= s:
-        return k
-    while True:
-        k += 1
-        current_fact *= k
-        current_sum = sum(int(d) for d in str(current_fact))
-        if current_sum >= s:
-            return k
-
-T = int(input())
-for _ in range(T):
-    s = int(input())
-    print(find_min_k(s))
+n = int(input())
+if n % 2 != 0:
+    print("NO")
+else:
+    print("YES")
+    for i in range(1, n, 2):
+        a = i
+        b = i + 1
+        print(a, b)
+        print(b, a)
