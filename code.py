70aa744d1af1869e807fa18ac743055037e4fd42
Analyzer <analyzer@test.com>
2026-02-09 13:49:58 -0800

    Update

--- code.py
+++ code.py
@@ -1,15 +1,7 @@
-import math
-
-n = int(input())
-a = list(map(int, input().split()))
-max_a = max(a)
-differences = [max_a - x for x in a]
-
-current_gcd = 0
-for d in differences:
-    current_gcd = math.gcd(current_gcd, d)
-
-z = current_gcd
-y = sum(d // z for d in differences)
-
-print(y, z)
+t = int(input())
+for _ in range(t):
+    a, b = map(int, input().split())
+    total = a + b
+    sum_limit = total // 3
+    min_ab = min(a, b)
+    print(min(sum_limit, min_ab))
