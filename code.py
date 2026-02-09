20166e6f136c4e173f639a8e10864e9cb476aa72
Analyzer <analyzer@test.com>
2026-02-09 10:23:29 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,25 @@
 n = int(input())
-a = list(map(int, input().split()))
 
-inversion_count = 0
-for i in range(n):
-    for j in range(i + 1, n):
-        if a[i] > a[j]:
-            inversion_count += 1
+def is_palindrome(num):
+    s = str(num)
+    return s == s[::-1]
 
-current_parity = inversion_count % 2
-
-m = int(input())
-for _ in range(m):
-    l, r = map(int, input().split())
-    length = r - l + 1
-    swaps = length // 2
-    if swaps % 2 == 1:
-        current_parity ^= 1
-    print("even" if current_parity == 0 else "odd")
+def is_prime(num):
+    if num <= 1:
+        return False
+    elif num <= 3:
+        return True
+    elif num % 2 == 0:
+        return False
+    d = num - 1
+    s = 0
+    while d % 2 == 0:
+        d //= 2
+        s += 1
+    bases = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]
+    for a in bases:
+        if a >= num:
+            continue
+        x = pow(a, d, num)
+        if x == 1 or x == num - 1:
+            cont
