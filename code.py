c8784c466e83481673283b2f2124878a4dea2c34
Analyzer <analyzer@test.com>
2026-02-09 11:23:11 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,18 @@
-def main():
+A_str = "What are you doing while sending "  # length 32
+B_str = " Are you busy? Will you send "      # length 29
+f0_str = "What are you doing at the end of the world? Are you busy? Will you save us?"
+
+def solve():
     import sys
+    input = sys.stdin.read().split()
+    q = int(input[0])
+    idx = 1
+    output = []
+    for _ in range(q):
+        n = int(input[idx])
+        k = int(input[idx+1])
+        idx +=2
 
-    prefix_str = "What are you doing while sending "
-    middle_str = " Are you busy? Will you send "
-    f0_str = "What are you doing at the end of the world? Are you busy? Will you save us?"
-
-    def compute_L(n):
-        if n == 0:
-            return 75
-        L = 75
-        for i in range(1, n + 1):
-            L = 2 * L + 67
-        return L
-
-    def get_char(n, k):
-        current_n = n
-        current_k = k
-        while True:
-            if current_n == 0:
-  
+        if n > 60:
+            # Proceed since L_n is way larger than k
+            c
