180625ce29b89b9363cf80a9a427cb1b3e7d2cad
Analyzer <analyzer@test.com>
2026-02-09 10:38:53 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,21 @@
-MOD = 1000009
+def has_subarray_sum(test_cases):
+    for case in test_cases:
+        M, Y, arr = case
+        current_sum = 0
+        seen = {0}
+        found = False
+        for num in arr:
+            current_sum += num
+            if (current_sum - Y) in seen:
+                found = True
+                break
+            seen.add(current_sum)
+        print("YES" if found else "NO")
 
 def main():
     import sys
-    h, w, p = map(int, sys.stdin.readline().split())
-    is_penalty = [[False]*(w+1) for _ in range(h+1)]
-    for _ in range(p):
-        row, col = map(int, sys.stdin.readline().split())
-        is_penalty[row][col] = True
-    
-    # Initialize DP table
-    dp = [[[0]*(p+1) for _ in range(w+1)] for __ in range(h+1)]
-    
-    # Starting cell (1,1)
-    if is_penalty[1][1]:
-        dp[1][1][1] = 1
-    else:
-        dp[1][1][0] = 1
-    
-    for i in range(1,
+    input = sys.stdin.read().split()
+    ptr = 0
+    K = int(input[ptr])
+    ptr += 1
+    test_cas
