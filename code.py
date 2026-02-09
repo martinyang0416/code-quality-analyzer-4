55c6d388c971da7e3570f64efb2944411838bf03
Analyzer <analyzer@test.com>
2026-02-09 15:11:43 -0800

    Update

--- code.py
+++ code.py
@@ -1,25 +1,12 @@
 import sys
-import math
 
-def get_gcd(arr):
-    from math import gcd
-    current_gcd = arr[0]
-    for num in arr[1:]:
-        current_gcd = gcd(current_gcd, num)
-        if current_gcd == 1:
-            break
-    return current_gcd
+def main():
+    T = int(sys.stdin.readline())
+    for _ in range(T):
+        n = sys.stdin.readline().strip()
+        total = sum(int(c) for c in n)
+        check = (10 - (total % 10)) % 10
+        print(n + str(check))
 
-def get_divisors(g):
-    if g == 0:
-        return []
-    factors = {}
-    i = 2
-    while i * i <= g:
-        while g % i == 0:
-            factors[i] = factors.get(i, 0) + 1
-            g = g // i
-        i += 1
-    if g > 1:
-        factors[g] = 1
-    divisors = [1]
+if __name__ == "__main__":
+    main()
