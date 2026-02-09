eded7b49211420c85f9f06d54ec11a95acc42259
Analyzer <analyzer@test.com>
2026-02-09 14:04:47 -0800

    Update

--- code.py
+++ code.py
@@ -1,24 +1,20 @@
-from collections import defaultdict
+n = int(input())
 
-def main():
-    import sys
-    input = sys.stdin.read().split()
-    N = int(input[0])
-    M = int(input[1])
-    A = list(map(int, input[2:2+N]))
-    
-    count_dict = defaultdict(int)
-    count_dict[0] = 1
-    current_sum = 0
-    result = 0
-    
-    for num in A:
-        current_sum += num
-        rem = current_sum % M
-        result += count_dict[rem]
-        count_dict[rem] += 1
-    
-    print(result)
+# Read set A
+a_input = list(map(int, input().split()))
+a = set(a_input[1:]) if a_input[0] > 0 else set()
 
-if __name__ == "__main__":
-    main()
+# Read set B
+b_input = list(map(int, input().split()))
+b = set(b_input[1:]) if b_input[0] > 0 else set()
+
+# Read set C
+c_input = list(map(int, input().split()))
+c = set(c_input[1:]) if c_input[0] > 0 else set()
+
+# Compute required sets
+set1 = c - a
+set2 = b & c
+
+# Calculate union and output the size
+print(len(set1.union(set2)))
