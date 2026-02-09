0748981446f36cdbaeaba0c34af7a72857b065ae
Analyzer <analyzer@test.com>
2026-02-09 10:24:51 -0800

    Update

--- code.py
+++ code.py
@@ -1,6 +1,19 @@
-n = int(input())
-if n < 2:
-    print(0)
-else:
-    result = (n - 1) * (n - 2) // 2
-    print(result)
+class DSU:
+    def __init__(self):
+        self.parent = {}
+    
+    def find(self, x):
+        if x not in self.parent:
+            self.parent[x] = x
+            return x
+        while self.parent[x] != x:
+            self.parent[x] = self.parent[self.parent[x]]  # Path compression
+            x = self.parent[x]
+        return x
+    
+    def union(self, x, y):
+        x_root = self.find(x)
+        y_root = self.find(y)
+        if x_root == y_root:
+            return
+        self.parent[y_root]
