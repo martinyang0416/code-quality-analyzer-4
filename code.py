2388b8059d75939f29401c8abe40114cbce3adea
Analyzer <analyzer@test.com>
2026-02-09 13:40:41 -0800

    Update

--- code.py
+++ code.py
@@ -1,18 +1,23 @@
-n = int(input())
-events = []
-for _ in range(n):
-    s, d = map(int, input().split())
-    end = s + d - 1
-    events.append((end, s))
-
-events.sort()
-
-count = 0
-last_end = 0
-
-for end, start in events:
-    if start > last_end:
-        count += 1
-        last_end = end
-
-print(count)
+def compute_max(even, odd, start_even):
+    e_ptr = 0
+    o_ptr = 0
+    sum_total = 0
+    current_parity = None
+    
+    if start_even:
+        if e_ptr >= len(even):
+            return 0
+        sum_total += even[e_ptr]
+        e_ptr += 1
+        current_parity = 0
+    else:
+        if o_ptr >= len(odd):
+            return 0
+        sum_total += odd[o_ptr]
+        o_ptr += 1
+        current_parity = 1
+    
+    while True:
+        if current_parity == 0:
+            # Next needs to be odd
+      
