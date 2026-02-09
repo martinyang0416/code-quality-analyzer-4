277372af77e01a323fdb9b67b30edf00cc1c2c85
Analyzer <analyzer@test.com>
2026-02-09 10:41:01 -0800

    Update

--- code.py
+++ code.py
@@ -1,17 +1,7 @@
-P = int(input())
-N = int(input())
-B = list(map(int, input().split()))
-K = int(input())
+n = int(input())
+arr = list(map(int, input().split()))
 
-# Initialize dp array where dp[s] represents the maximum product for sum s
-# We only need sums up to K
-max_possible_sum = K
-dp = [0] * (max_possible_sum + 1)
-dp[0] = 1  # Base case: empty subset
-
-for x in B:
-    # Iterate from high to low to avoid using the same element multiple times
-    for s in range(max_possible_sum, x - 1, -1):
-        if dp[s - x] != 0:
-            current_product = dp[s - x] * x
-      
+product = 1
+for num in arr:
+    product *= num
+print(product)
