4d867d93c206954123371824fbf1f8d34bcc6de5
Analyzer <analyzer@test.com>
2026-02-09 13:03:16 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,27 @@
-import sys
-from sys import stdin
-from collections import deque
+n, k = map(int, input().split())
 
-def main():
-    sys.setrecursionlimit(1 << 25)
-    n, m = map(int, stdin.readline().split())
-    h = list(map(int, stdin.readline().split()))
-    adj = [[] for _ in range(n)]
-    in_degree = [0] * n
-    for _ in range(m):
-        u, v = map(int, stdin.readline().split())
-        u -= 1
-        v -= 1
-        adj[u].append(v)
-        in_degree[v] += 1
+type10 = []
+type01 = []
+type11 = []
 
-    # Compute topological order
-    queue = deque()
-    for i in range(n):
-       
+for _ in range(n):
+    t, a, b = map(int, input().split())
+    if a == 1 and b == 1:
+        type11.append(t)
+    elif a == 1 and b == 0:
+        type10.append(t)
+    elif a == 0 and b == 1:
+        type01.append(t)
+
+# Sort each type and compute prefix sums
+type10.sort()
+type01.sort()
+type11.sort()
+
+prefix10 = [0]
+for t in type10:
+    prefix10.append(prefix10[-1] + t)
+
+prefix01 = [0]
+for t in type01:
+    prefix01.append(prefi
