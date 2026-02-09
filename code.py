cfd0ccb1af7ff02daaa25d1da3cb3a5826b1f95a
Analyzer <analyzer@test.com>
2026-02-09 14:58:00 -0800

    Update

--- code.py
+++ code.py
@@ -1,17 +1,19 @@
-import collections
+def maxScoreWords(words, letters, score):
+    from collections import defaultdict
 
-def validateBinaryTreeNodes(n, leftChild, rightChild):
-    # Check if any node has the same non-null left and right child
-    for i in range(n):
-        if leftChild[i] != -1 and leftChild[i] == rightChild[i]:
-            return False
-    
-    # Collect all child nodes to determine the root
-    children = set()
-    for i in range(n):
-        l, r = leftChild[i], rightChild[i]
-        if l != -1:
-            children.add(l)
-        if r != -1:
-            children.add(r)
-    
+    # Precompute the count of each letter available
+    letters_count = [0] * 26
+    for c in letters:
+        letters_count[ord(c) - ord('a')] += 1
+
+    # Precompute the count of each character for every word
+    word_counts = []
+    for word in words:
+        cnt = [0] * 26
+        for c in word:
+            cnt[ord(c) - ord('a')] += 1
+        word_counts.append(cnt)
+
+    max_score = 0
+    n = len(words)
+    # I
