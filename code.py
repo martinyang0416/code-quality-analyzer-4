4b32ccb5cdea3e8bd0105244a4b457698243a274
Analyzer <analyzer@test.com>
2026-02-09 10:12:49 -0800

    Update

--- code.py
+++ code.py
@@ -1,31 +1,6 @@
-n, k = map(int, input().split())
-s = input().strip()
-
-if n == 0:
-    print(0)
-    exit()
-
-# Create runs list
-runs = []
-current = s[0]
-count = 1
-for c in s[1:]:
-    if c == current:
-        count += 1
-    else:
-        runs.append((current, count))
-        current = c
-        count = 1
-runs.append((current, count))
-
-max_len = 0
-current_sum = 0
-b_count = 0
-left = 0
-
-for right in range(len(runs)):
-    char, length = runs[right]
-    if char == 'b':
-        b_count += 1
-    current_sum += length
-    
+n = int(input())
+scores = list(map(int, input().split()))
+min_score = min(scores)
+third_student_score = scores[2]
+modified_score = (min_score ^ third_student_score) + 5
+print(modified_score)
