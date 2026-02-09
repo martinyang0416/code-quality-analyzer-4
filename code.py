f836714d963d74b2e128fa3c713a5f854850a664
Analyzer <analyzer@test.com>
2026-02-09 11:16:38 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,3 @@
-a, b, n = map(int, input().split())
-
-def fib(m):
-    if m == 0:
-        return 0
-    elif m == 1:
-        return 1
-    a, b = 0, 1
-    for _ in range(2, m + 1):
-        c = a + b
-        a = b
-        b = c
-    return b
-
-f_n_minus_1 = fib(n - 1)
-f_n = fib(n)
-
-result = a * f_n_minus_1 + b * f_n
-print(result)
+s = input().strip()
+last_digit = int(s[-1])
+print(1 if last_digit % 2 else 0)
