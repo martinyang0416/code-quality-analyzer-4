6e41bd7346235121ae0cb6970d78cba1179ec869
Analyzer <analyzer@test.com>
2026-02-09 10:24:01 -0800

    Update

--- code.py
+++ code.py
@@ -1,25 +1,18 @@
-n = int(input())
+import sys
 
-def is_palindrome(num):
-    s = str(num)
-    return s == s[::-1]
-
-def is_prime(num):
-    if num <= 1:
-        return False
-    elif num <= 3:
-        return True
-    elif num % 2 == 0:
-        return False
-    d = num - 1
-    s = 0
-    while d % 2 == 0:
-        d //= 2
-        s += 1
-    bases = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]
-    for a in bases:
-        if a >= num:
-            continue
-        x = pow(a, d, num)
-        if x == 1 or x == num - 1:
-            cont
+def main():
+    n, V = map(int, sys.stdin.readline().split())
+    w = list(map(int, sys.stdin.readline().split()))
+    v = list(map(int, sys.stdin.readline().split()))
+    
+    deltas = [vi - V for vi in v]
+    
+    dp = {0: 1}  # Initialize with the empty subset
+    
+    for delta in deltas:
+        new_dp = {}
+        for current_sum in dp:
+            count = dp[current_sum]
+            # Option 1: not taking the current delta
+            if current_sum in new_dp:
+                
