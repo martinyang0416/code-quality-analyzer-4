a7b3a4bc722adf59c27f146eed5e16a102063f7f
Analyzer <analyzer@test.com>
2026-02-09 12:17:34 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,19 @@
+import sys
+import time
+import itertools
+from itertools import accumulate, product, permutations, combinations
+import collections
+from collections import Counter, OrderedDict, deque, defaultdict, ChainMap
+from functools import lru_cache
 import math
-from sys import stdin
-from sys import setrecursionlimit
-setrecursionlimit(100000)
-def put():  return  map(int, stdin.readline().split())
-for _ in range(int(input())):
-	n=int(input())
-	if(n%3==0):
-		print(n//3,0,0)
-	elif(n%3==1):
-		if(n//3>=2):
-			print((n//3)-2,0,1)
-		else:
-			print(-1)
-	else:
-		if(n//3>=1):
-			print((n//3)-1,1,0)
-		else:
-			print(-1)
+from math import sqrt, sin, cos, tan, ceil, fabs, floor, gcd, exp, log, log2
+import fractions
+from typing import List, Tuple
+import numpy as np
+import random
+import heapq
+from heapq import *
+from dataclasses import dataclass
 
+import builtins
+import re
