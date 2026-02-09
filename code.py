2401171635751c7c6e20fba8860eff208662f8d3
Analyzer <analyzer@test.com>
2026-02-09 13:22:16 -0800

    Update

--- code.py
+++ code.py
@@ -1,17 +1,19 @@
-T = int(input())
-for _ in range(T):
-    N, M = map(int, input().split())
-    A = list(map(int, input().split()))
-    B = list(map(int, input().split()))
-    i = j = 0
-    merged = []
-    while i < N and j < M:
-        if A[i] >= B[j]:
-            merged.append(A[i])
-            i += 1
+import sys
+
+def main():
+    data = sys.stdin.read().split()
+    t = int(data[0])
+    res = []
+    idx = 1
+    for _ in range(t):
+        n = int(data[idx])
+        k = int(data[idx + 1])
+        idx += 2
+        if (n - 1) % (k + 1) == 0:
+            res.append("Nandu")
         else:
-            merged.append(B[j])
-            j += 1
-    merged.extend(A[i:])
-    merged.extend(B[j:])
-    print(' '.join(map(str, merged)))
+            res.append("Rani")
+    print('\n'.join(res))
+
+if __name__ == "__main__":
+    main()
