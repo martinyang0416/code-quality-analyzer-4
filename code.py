1fe9bac6319d6af9d2fe5e3a7117945fc0d0078f
Analyzer <analyzer@test.com>
2026-02-09 12:05:21 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,23 @@
-import sys
-import time
-import itertools
-from itertools import accumulate, product, permutations, combinations
-import collections
-from collections import Counter, OrderedDict, deque, defaultdict, ChainMap
-from functools import lru_cache
-import math
-from math import sqrt, sin, cos, tan, ceil, fabs, floor, gcd, exp, log, log2
-import fractions
-from typing import List, Tuple
-import numpy as np
-import random
-import heapq
-from heapq import *
-from dataclasses import dataclass
+def b_uniqueness(arr):
+    i = 0
+    j = len(arr) - 1
 
-import builtins
-import re
+    last_pos = {}
+    for ind, elem in enumerate(arr):
+        last_pos[elem] = ind
+
+    repeated_set = set()
+    while arr[j] not in repeated_set:
+        repeated_set.add(arr[j])
+        j -= 1
+
+    repeated_set.clear()
+    ans = j + 1
+    while arr[i] not in repeated_set:
+        repeated_set.add(arr[i])
+        j = max(j, last_pos[arr[i]])
+        ans = min(ans, j - i)
+        i += 1
+        if i == len(arr):
+            break
+    return
