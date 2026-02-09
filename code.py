64c2eb0cbbc67e1f81a839591b7ce488489ef66e
Analyzer <analyzer@test.com>
2026-02-09 13:29:31 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,14 @@
 import sys
-from sys import stdin
-from collections import deque
 
-def main():
-    n, m, h = map(int, stdin.readline().split())
-    u = list(map(int, stdin.readline().split()))
-    adj = [[] for _ in range(n)]
-    for _ in range(m):
-        a, b = map(int, stdin.readline().split())
-        a -= 1
-        b -= 1
-        delta = (u[b] - u[a]) % h
-        if delta == 1:
-            adj[a].append(b)
-        elif delta == h - 1:
-            adj[b].append(a)
-    
-    # Kosaraju's algorithm to find SCCs
- 
+def count_zeros(n):
+    count = 0
+    divisor = 5
+    while divisor <= n:
+        count += n // divisor
+        divisor *= 5
+    return count
+
+t = int(sys.stdin.readline())
+for _ in range(t):
+    n = int(sys.stdin.readline())
+    print(count_zeros(n))
