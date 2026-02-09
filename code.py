e5bd9e8dfeb8792a2f1ba637833ab64d83ce4853
Analyzer <analyzer@test.com>
2026-02-09 12:32:36 -0800

    Update

--- code.py
+++ code.py
@@ -1,8 +1,19 @@
-def maxTeams(a,b):
-    ans=min((a+b)//3,min(a,b))
-    return ans
+import sys
+import time
+import itertools
+from itertools import accumulate, product, permutations, combinations
+import collections
+from collections import Counter, OrderedDict, deque, defaultdict, ChainMap
+from functools import lru_cache
+import math
+from math import sqrt, sin, cos, tan, ceil, fabs, floor, gcd, exp, log, log2
+import fractions
+from typing import List, Tuple
+import numpy as np
+import random
+import heapq
+from heapq import *
+from dataclasses import dataclass
 
-inputArray=input().strip().split()
-a=int(inputArray[0])
-b=int(inputArray[1])
-print(maxTeams(a,b))
+import builtins
+import re
