42a259e028a7970d4abcb3208e30135ce23f4dc6
Analyzer <analyzer@test.com>
2026-02-09 11:33:42 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,16 @@
 import sys
 
 def main():
+    sys.setrecursionlimit(1 << 25)
     N = int(sys.stdin.readline())
     a = list(map(int, sys.stdin.readline().split()))
-    
-    # Compute prefix sums
     prefix = [0] * (N + 1)
-    for i in range(1, N + 1):
-        prefix[i] = prefix[i-1] + a[i-1]
-    
-    subarrays = []
-    for l in range(1, N + 1):
-        for r in range(l, N + 1):
-            s = prefix[r] - prefix[l-1]
-            subarrays.append((s, l, r))
-    
-    for i in range(1, N + 1):
-        s_in = []
-        s_out = []
-        for s, l, r 
+    for i in range(N):
+        prefix[i+1] = prefix[i] + a[i]
+
+    for i in range(1, N+1):
+        # Compute A: subarrays including i
+        A = [prefix[r] - prefix[l-1] for l in range(1, i+1) for r in range(i, N+1)]
+        # Compute B: subarrays not including i
+        B = []
+        # Left part: subarrays entirely before 
