9e9fe528303302510e2321e3fc567697868e6d77
Analyzer <analyzer@test.com>
2026-02-09 13:30:02 -0800

    Update

--- code.py
+++ code.py
@@ -1,14 +1,7 @@
-import sys
-
-def count_zeros(n):
-    count = 0
-    divisor = 5
-    while divisor <= n:
-        count += n // divisor
-        divisor *= 5
-    return count
-
-t = int(sys.stdin.readline())
-for _ in range(t):
-    n = int(sys.stdin.readline())
-    print(count_zeros(n))
+n = int(input())
+marks = list(map(int, input().split()))
+ranks = []
+for m in marks:
+    count = sum(1 for x in marks if x > m)
+    ranks.append(str(count + 1))
+print(' '.join(ranks))
