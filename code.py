2777f4504f7b888acefbcf5ffd17f9cea6e3563a
Analyzer <analyzer@test.com>
2026-02-09 14:10:07 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,14 @@
-class FenwickTree:
-    def __init__(self, size):
-        self.n = size
-        self.tree_w = [0] * (self.n + 1)
-        self.tree_v = [0] * (self.n + 1)
-    
-    def update(self, idx, delta_w, delta_v):
-        idx += 1
-        while idx <= self.n:
-            self.tree_w[idx] += delta_w
-            self.tree_v[idx] += delta_v
-            idx += idx & -idx
-    
-    def query(self, idx):
-        idx += 1
-        sum_w = 0
-        sum_v = 0
-        while idx > 0:
-            sum_w += self.tree_w[i
+y, k, n = map(int, input().split())
+
+start = (y + 1 + k - 1) // k * k
+max_s = (n // k) * k
+
+if start > max_s:
+    print(-1)
+else:
+    res = []
+    current = start
+    while current <= max_s:
+        res.append(current - y)
+        current += k
+    print(' '.join(map(str, res)))
