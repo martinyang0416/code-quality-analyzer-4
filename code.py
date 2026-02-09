81f3a124ff0a9be7b0a194ea86f93880141e01d8
Analyzer <analyzer@test.com>
2026-02-09 10:52:10 -0800

    Update

--- code.py
+++ code.py
@@ -1,34 +1,4 @@
+import math
+
 a = int(input())
-presidents = [
-    "Washington",
-    "Adams",
-    "Jefferson",
-    "Madison",
-    "Monroe",
-    "Adams",
-    "Jackson",
-    "Van Buren",
-    "Harrison",
-    "Tyler",
-    "Polk",
-    "Taylor",
-    "Fillmore",
-    "Pierce",
-    "Buchanan",
-    "Lincoln",
-    "Johnson",
-    "Grant",
-    "Hayes",
-    "Garfield",
-    "Arthur",
-    "Cleveland",
-    "Harrison",
-    "Cleveland",
-    "McKinley",
-    "Roosevelt",
-    "Taft",
-    "Wilson",
-    "Harding",
-    "Coolidge",
-    "Hoover",
-    "
+print(math.isqrt(a))
