c7e9cea792d3165972281af2323e0dc8b2a92960
Analyzer <analyzer@test.com>
2026-02-09 10:33:14 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,25 @@
 import bisect
 
-def minimal_operations():
-    n, m = map(int, input().split())
-    seq1 = list(map(int, input().split()))
-    seq2 = list(map(int, input().split()))
-    
-    seq1.sort()
-    seq2.sort()
-    
-    prefix1 = [0] * (n + 1)
-    for i in range(n):
-        prefix1[i + 1] = prefix1[i] + seq1[i]
-    
-    prefix2 = [0] * (m + 1)
-    for i in range(m):
-        prefix2[i + 1] = prefix2[i] + seq2[i]
-    
-    def compute_cost(X):
-        # Calculate cost for seq1: sum(max(0, a - X) for a in seq
+def main():
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+    idx = 0
+    c = int(data[idx])
+    idx +=1
+    results = []
+    for _ in range(c):
+        n = int(data[idx])
+        m = int(data[idx+1])
+        T = int(data[idx+2])
+        idx +=3
+        p = list(map(int, data[idx:idx+n]))
+        idx +=n
+        
+        p.sort()
+        prefix = [0]
+        current = 0
+        for num in p:
+            current += num
+            prefix.append(current)
+     
