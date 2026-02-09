9674430aac7da4d275fc5f8dc85dfa0ed5325aa4
Analyzer <analyzer@test.com>
2026-02-09 14:31:36 -0800

    Update

--- code.py
+++ code.py
@@ -1,15 +1,8 @@
-from collections import deque
-from typing import List
-
-class Solution:
-    def maxCandies(self, status: List[int], candies: List[int], keys: List[List[int]], containedBoxes: List[List[int]], initialBoxes: List[int]) -> int:
-        available_boxes = set(initialBoxes)
-        available_keys = set()
-        processed = set()
-        queue = deque()
-        
-        for box in initialBoxes:
-            if status[box] == 1 or box in available_keys:
-                queue.append(box)
-        
-        
+def numTimesAllBlue(light):
+    current_max = 0
+    count = 0
+    for i in range(len(light)):
+        current_max = max(current_max, light[i])
+        if current_max == i + 1:
+            count += 1
+    return count
