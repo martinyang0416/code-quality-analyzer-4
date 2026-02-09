53bdcf70fd29f7f69286665675d3493d702193b7
Analyzer <analyzer@test.com>
2026-02-09 10:34:25 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,20 @@
-def main():
-    import sys
-    N, K, T, U = map(int, sys.stdin.readline().split())
-    H = list(map(int, sys.stdin.readline().split()))
-    
-    cut_list = []
-    for h in H:
-        if h < T:
-            cut_list.append(h)
-    
-    max_allowed = N - K
-    if len(cut_list) <= max_allowed:
-        print(len(cut_list))
-        return
-    
-    # Re-evaluation process
-    # Start from the end of the cut_list and check in reverse
-    i = len(cut_list) - 1
-    while i >= 0 and len(cut_list) > max_allo
+MOD = 10**9 + 7
+
+n, T = map(int, input().split())
+movies = [tuple(map(int, input().split())) for _ in range(n)]
+
+max_mask = 1 << n
+# Initialize DP table with all zeros
+dp = [[[0] * (T + 1) for _ in range(5)] for _ in range(max_mask)]
+
+for i in range(n):
+    t_i, c_i = movies[i]
+    if t_i <= T:
+        mask = 1 << i
+        dp[mask][c_i][t_i] += 1
+
+for mask in range(max_mask):
+    for last in range(1, 5):
+        for time in range(T + 1):
+            if dp[mask][last][time] == 0:
+               
