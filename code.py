01b9038aa6b18a20e14729b009ac7175d0492368
Analyzer <analyzer@test.com>
2026-02-09 11:06:06 -0800

    Update

--- code.py
+++ code.py
@@ -1,5 +1,10 @@
-a = input().strip()
-if any(c in {'2', '3', '5', '7'} for c in a):
-    print(1)
-else:
-    print(0)
+a = int(input())
+found = 0
+n = a
+while n > 0:
+    d = n % 10
+    if d % 3 == 0:
+        found = 1
+        break
+    n = n // 10
+print(found)
