3a833d9c8b5d4b08c22886b7289419cffed22553
Analyzer <analyzer@test.com>
2026-02-09 15:01:07 -0800

    Update

--- code.py
+++ code.py
@@ -1,15 +1,9 @@
-from functools import lru_cache
-
-class Solution:
-    def longestDecomposition(self, text: str) -> int:
-        n = len(text)
-        
-        @lru_cache(maxsize=None)
-        def helper(start, end):
-            if start > end:
-                return 0
-            max_k = 1  # Default is the entire substring as one part
-            max_possible_l = (end - start + 1) // 2
-            for l in range(1, max_possible_l + 1):
-                if text[start:start+l] == text[end - l + 1:end + 1]:
-       
+def minOperations(target):
+    res = 0
+    prev = 0
+    for num in target:
+        diff = num - prev
+        if diff > 0:
+            res += diff
+        prev = num
+    return res
