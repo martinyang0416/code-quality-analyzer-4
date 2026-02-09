b7c05b453792ea8711193543d6d1055448019119
Analyzer <analyzer@test.com>
2026-02-09 11:33:20 -0800

    Update

--- code.py
+++ code.py
@@ -3,17 +3,19 @@ import sys
 def main():
     N = int(sys.stdin.readline())
     a = list(map(int, sys.stdin.readline().split()))
-    prefix = [0]
-    for num in a:
-        prefix.append(prefix[-1] + num)
     
-    all_sums = []
-    for l in range(N):
-        for r in range(l, N):
-            s = prefix[r+1] - prefix[l]
-            all_sums.append((s, l, r))
+    # Compute prefix sums
+    prefix = [0] * (N + 1)
+    for i in range(1, N + 1):
+        prefix[i] = prefix[i-1] + a[i-1]
     
-    for i in range(N):
-        A = [s for s, l, r in all_sums if l <= i <= r]
-        B1_sums = [s for s, l, r in all_sums if r < i]
-        B2_sums = [s 
+    subarrays = []
+    for l in range(1, N + 1):
+        for r in range(l, N + 1):
+            s = prefix[r] - prefix[l-1]
+            subarrays.append((s, l, r))
+    
+    for i in range(1, N + 1):
+        s_in = []
+        s_out = []
+        for s, l, r 
