a352795a2c86461d320c0461f8142b5c430ecae6
Analyzer <analyzer@test.com>
2026-02-09 15:02:29 -0800

    Update

--- code.py
+++ code.py
@@ -1,12 +1,20 @@
-def reverseParentheses(s: str) -> str:
-    stack = []
-    current_str = ''
-    for char in s:
-        if char == '(':
-            stack.append(current_str)
-            current_str = ''
-        elif char == ')':
-            current_str = stack.pop() + current_str[::-1]
-        else:
-            current_str += char
-    return current_str
+import sys
+from collections import deque
+
+def main():
+    N, M = map(int, sys.stdin.readline().split())
+    adj = [[] for _ in range(N+1)]
+    for _ in range(M):
+        u, v, c = map(int, sys.stdin.readline().split())
+        adj[u].append((v, c))
+        adj[v].append((u, c))
+    
+    # BFS to find spanning tree
+    parent = [0] * (N + 1)
+    edge_c = [0] * (N + 1)
+    visited = [False] * (N + 1)
+    q = deque()
+    q.append(1)
+    visited[1] = True
+    parent[1] = 0
+    edge_c[1] = 0  # dummy
