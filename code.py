3e1b1322f7740d810fbf123dd2206c47e9a3cd62
Analyzer <analyzer@test.com>
2026-02-09 14:22:14 -0800

    Update

--- code.py
+++ code.py
@@ -1,11 +1,10 @@
-class Solution:
-    def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
-        costs = [abs(ord(sc) - ord(tc)) for sc, tc in zip(s, t)]
-        left = current_sum = max_len = 0
-        for right in range(len(costs)):
-            current_sum += costs[right]
-            while current_sum > maxCost:
-                current_sum -= costs[left]
-                left += 1
-            max_len = max(max_len, right - left + 1)
-        return max_len
+def uniquePaths(m, n):
+    a = m + n - 2
+    k = min(m - 1, n - 1)
+    numerator = 1
+    for i in range(k):
+        numerator *= (a - i)
+    denominator = 1
+    for i in range(1, k + 1):
+        denominator *= i
+    return numerator // denominator
