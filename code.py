3c0c5467228319fed9d41963fd6890ce7822425f
Analyzer <analyzer@test.com>
2026-02-09 15:08:58 -0800

    Update

--- code.py
+++ code.py
@@ -1,23 +1,10 @@
-import sys
-
-def main():
-    input = sys.stdin.read().split()
-    idx = 0
-    T = int(input[idx])
-    idx += 1
-    for _ in range(T):
-        N, K = int(input[idx]), int(input[idx+1])
-        idx +=2
-        A = list(map(int, input[idx:idx+N]))
-        idx += N
-        
-        found = False
-        if K in A:
-            print("YES")
-            continue
-        
-        for i in range(N):
-            m = A[i]
-            if m > K:
-                continue
-            new_list = A[:i] + A[i+1:]
+# Read the number of test cases
+t = int(input())
+for _ in range(t):
+    a = input().strip()
+    b = input().strip()
+    # Check if there's any common character
+    if set(a) & set(b):
+        print("Yes")
+    else:
+        print("No")
