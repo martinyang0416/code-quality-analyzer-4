028fefbf69c805b0cdece838eaf0be80cdfbdc23
Analyzer <analyzer@test.com>
2026-02-09 12:06:39 -0800

    Update

--- code.py
+++ code.py
@@ -1,28 +1,19 @@
-n, k = map(int, input().split())
-seat = {4:n, 2:n*2, 1:0}
-extra1 = 0
-a = sorted(map(int, input().split()), reverse=True)
+import sys
+import time
+import itertools
+from itertools import accumulate, product, permutations, combinations
+import collections
+from collections import Counter, OrderedDict, deque, defaultdict, ChainMap
+from functools import lru_cache
+import math
+from math import sqrt, sin, cos, tan, ceil, fabs, floor, gcd, exp, log, log2
+import fractions
+from typing import List, Tuple
+import numpy as np
+import random
+import heapq
+from heapq import *
+from dataclasses import dataclass
 
-def sit(n, m):
-    num = min(seat[n], m)
-    seat[n] -= num
-    return m - num
-
-for m in a:
-    p4 = m // 4
-    p3, p2, p1 = 0, 0, 0
-    if m%4 == 3:
-        p3 = 1
-    else:
-        p2 = int(m % 4 > 1)
-        p1 = int(m % 2)
-
-    extra4 = sit(4, p4)
-    p2 += extra4*2
-    if sit(4, p3) > 0:
-        p2 += 1
-        p1 += 1
-
-    extra2 = sit(2, p2)
-    x = sit(4, extra2)
-    
+import builtins
+import re
