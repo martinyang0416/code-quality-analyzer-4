f50683ba442c8f87ffff37d1caa232042b2e21d3
Analyzer <analyzer@test.com>
2026-02-09 10:39:45 -0800

    Update

--- code.py
+++ code.py
@@ -1,23 +1,9 @@
-import bisect
-from collections import defaultdict
-
-def main():
-    import sys
-    input = sys.stdin.read().split()
-    ptr = 0
-    T = int(input[ptr])
-    ptr +=1
-    for _ in range(T):
-        S = input[ptr]
-        ptr +=1
-        Q = int(input[ptr])
-        ptr +=1
-        Ts = list(map(int, input[ptr:ptr+Q]))
-        ptr += Q
-        
-        n = len(S)
-        prefix = [0] * (n + 1)
-        for i in range(1, n+1):
-            if S[i-1] == 'a':
-                prefix[i] = prefix[i-1] + 1
-   
+s = input().strip()
+result = []
+for c in s:
+    digit = int(c)
+    if digit % 2 == 0:
+        result.append(f'({c})')
+    else:
+        result.append(c)
+print(''.join(result))
