7a116e2f9182165f0e72d55472cd8f7ed6a0a413
Analyzer <analyzer@test.com>
2026-02-09 14:00:00 -0800

    Update

--- code.py
+++ code.py
@@ -1,16 +1,22 @@
-import sys
+vp = int(input())
+vd = int(input())
+t = int(input())
+f = int(input())
+c = int(input())
 
-def main():
-    t = int(sys.stdin.readline())
-    for _ in range(t):
-        n, m, k = map(int, sys.stdin.readline().split())
-        arr = list(map(int, sys.stdin.readline().split()))
-        c = min(k, m-1)
-        s = (m-1) - c
-        max_x = 0
-        for a in range(c + 1):
-            current_min = float('inf')
-            for a_prime in range(s + 1):
-                left = a + a_prime
-                right = left + (n - m)
-                current_max = max(arr[left], arr[right
+if vp >= vd:
+    print(0)
+else:
+    current = vp * t
+    if current >= c:
+        print(0)
+    else:
+        bijous = 0
+        while True:
+            time_catch = current / (vd - vp)
+            position_meet = current + vp * time_catch
+            if position_meet >= c:
+                break
+            bijous += 1
+            time_away = (position_meet / vd) + f
+            current = position_meet + vp * 
