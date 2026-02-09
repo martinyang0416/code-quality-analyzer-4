a1d76fd2c0cee90f73e3b4cc80f258c6da89b03a
Analyzer <analyzer@test.com>
2026-02-09 13:30:32 -0800

    Update

--- code.py
+++ code.py
@@ -1,7 +1,22 @@
-n = int(input())
-marks = list(map(int, input().split()))
-ranks = []
-for m in marks:
-    count = sum(1 for x in marks if x > m)
-    ranks.append(str(count + 1))
-print(' '.join(ranks))
+n, p = map(int, input().split())
+
+def factorize(x):
+    factors = {}
+    i = 2
+    while i * i <= x:
+        while x % i == 0:
+            factors[i] = factors.get(i, 0) + 1
+            x = x // i
+        i += 1
+    if x > 1:
+        factors[x] = 1
+    return factors
+
+factors = factorize(p)
+result = 1
+for prime, exp in factors.items():
+    cnt = exp // n
+    if cnt > 0:
+        result *= prime ** cnt
+
+print(result)
