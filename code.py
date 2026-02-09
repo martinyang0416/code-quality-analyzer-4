5bcb654d218afdd7c3a7a9a37822182a56420177
Analyzer <analyzer@test.com>
2026-02-09 11:28:59 -0800

    Update

--- code.py
+++ code.py
@@ -1,16 +1,21 @@
-def putaway(A, B, T, X, Y, W, S):
-    class Edge:
-        __slots__ = ['to', 'rev', 'capacity']
-        def __init__(self, to, rev, capacity):
-            self.to = to
-            self.rev = rev
-            self.capacity = capacity
+import bisect
 
-    class Dinic:
-        def __init__(self, n):
-            self.size = n
-            self.graph = [[] for _ in range(n)]
-        
-        def add_edge(self, fr, to, cap):
-            forward = Edge(to, len(self.graph[to]), cap)
-            backward = Edge(fr, len(se
+def putaway(A, B, T, X, Y, W, S):
+    X_sorted = sorted(X)
+    Y_sorted = sorted(Y)
+    
+    can_weak = []
+    can_small = []
+    
+    for w in W:
+        idx = bisect.bisect_right(X_sorted, w)
+        can_weak.append(idx < len(X_sorted))
+    
+    for s in S:
+        idx = bisect.bisect_right(Y_sorted, s)
+        can_small.append(idx < len(Y_sorted))
+    
+    # Check if any toy can't be assigned to any
+    for i in range(T):
+        if not (can_weak[i] or can_small[i]):
+          
