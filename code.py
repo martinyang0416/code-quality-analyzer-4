e8a9874e6bd8397b332df17cb1c27a70b4827bca
Analyzer <analyzer@test.com>
2026-02-09 14:33:32 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,9 @@
-from itertools import product
+import collections
 
-def flipLights(n, m):
-    if n == 0:
-        return 0
-    present_classes = set()
-    for i in range(1, n + 1):
-        parity = i % 2
-        mod3 = (i - 1) % 3
-        present_classes.add((parity, mod3))
-    present_classes = sorted(present_classes)
-    unique_states = set()
-    
-    for a, b, c, d in product([0, 1], repeat=4):
-        s = a + b + c + d
-        if s > m or (m - s) % 2 != 0:
-            continue
-        masks = []
-        for (p, m3) in present_cl
+def leastInterval(tasks, n):
+    freq = collections.Counter(tasks)
+    max_freq = max(freq.values(), default=0)
+    max_count = sum(1 for v in freq.values() if v == max_freq)
+    part1 = (max_freq - 1) * (n + 1) + max_count
+    part2 = len(tasks)
+    return max(part1, part2)
