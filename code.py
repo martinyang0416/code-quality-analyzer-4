735632726e8b77c1809d29bd6d1b26a04f647d59
Analyzer <analyzer@test.com>
2026-02-09 14:51:25 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,8 @@
-def longestPalindrome(s):
-    if not s:
-        return ""
-    start = 0
-    end = 0
-    n = len(s)
-    for i in range(n):
-        # Check for odd length palindromes
-        l, r = i, i
-        while l >= 0 and r < n and s[l] == s[r]:
-            l -= 1
-            r += 1
-        current_len = r - l - 1
-        if current_len > end - start:
-            start = l + 1
-            end = r - 1
-        
-        # Check for even length palindromes
-        l, r = i, i + 1
-        while l >= 0 and r < n 
+MOD = 10**9 + 7
+
+class Solution:
+    def countOrders(self, n: int) -> int:
+        res = 1
+        for i in range(1, n+1):
+            res = res * i * (2*i - 1) % MOD
+        return res
