1d78b292eb5561500bae80fbe8914671d175ff2a
Analyzer <analyzer@test.com>
2026-02-09 14:47:04 -0800

    Update

--- code.py
+++ code.py
@@ -1,17 +1,16 @@
-def getMaxRepetitions(s1, n1, s2, n2):
-    len_s2 = len(s2)
-    if len_s2 == 0:
-        return 0
-    
-    # Precompute transitions for each starting position in s2
-    transition = {}
-    for p_start in range(len_s2):
-        current_p = p_start
-        count = 0
-        for c in s1:
-            if c == s2[current_p]:
-                current_p += 1
-                if current_p == len_s2:
-                    count += 1
-                    current_p = 0
-        transition[p_start] = (count, curren
+def count_digit_one(n):
+    count = 0
+    i = 0
+    while 10**i <= n:
+        divisor = 10 ** (i + 1)
+        higher = n // divisor
+        current = (n // (10**i)) % 10
+        lower = n % (10**i)
+        if current < 1:
+            count += higher * (10**i)
+        elif current == 1:
+            count += higher * (10**i) + lower + 1
+        else:
+            count += (higher + 1) * (10**i)
+        i += 1
+    return count
