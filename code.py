45e26910ad38d013c94538cfce040816df9470d1
Analyzer <analyzer@test.com>
2026-02-09 14:04:08 -0800

    Update

--- code.py
+++ code.py
@@ -1,8 +1,24 @@
-L = int(input())
-N = int(input())
-for _ in range(N):
-    W, H = map(int, input().split())
-    if W < L or H < L:
-        print("UPLOAD ANOTHER")
-    else:
-        print("ACCEPTED" if W == H else "CROP IT")
+from collections import defaultdict
+
+def main():
+    import sys
+    input = sys.stdin.read().split()
+    N = int(input[0])
+    M = int(input[1])
+    A = list(map(int, input[2:2+N]))
+    
+    count_dict = defaultdict(int)
+    count_dict[0] = 1
+    current_sum = 0
+    result = 0
+    
+    for num in A:
+        current_sum += num
+        rem = current_sum % M
+        result += count_dict[rem]
+        count_dict[rem] += 1
+    
+    print(result)
+
+if __name__ == "__main__":
+    main()
