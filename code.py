b54d772439ea8d2afa43874d5943e1ffd0704e8a
Analyzer <analyzer@test.com>
2026-02-09 13:43:41 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,7 @@
-import sys
-
-class SegmentTreeNode:
-    def __init__(self, start, end):
-        self.start = start
-        self.end = end
-        self.left = None
-        self.right = None
-        self.sorted_li = []
-        self.prefix_max = []
-
-def build_tree(ropes, start, end):
-    node = SegmentTreeNode(start, end)
-    if start == end:
-        li, ri = ropes[start]
-        node.sorted_li = [(li, ri)]
-        node.prefix_max = [ri]
-    else:
-        mid = (start + end) // 2
-        node.left = build_tree(rope
+a = int(input())
+b = int(input())
+d = abs(a - b)
+if d % 2 == 0:
+    print(d * (d + 2) // 4)
+else:
+    print((d + 1) ** 2 // 4)
