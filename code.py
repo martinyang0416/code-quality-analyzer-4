b442a8186f4f9fa3451da12ad13c1c32d8ad7ac9
Analyzer <analyzer@test.com>
2026-02-09 13:23:57 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,15 @@
-n, m = map(int, input().split())
+from collections import defaultdict
 
-if m == 0:
-    print(0 if n == 1 else -1)
-else:
-    parent = list(range(n + 1))  # 1-based indexing
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
 
-    def find(u):
-        while parent[u] != u:
-            parent[u] = parent[parent[u]]  # Path compression
-            u = parent[u]
-        return u
+freq = defaultdict(int)
+count = 0
 
-    def union(u, v):
-        u_root = find(u)
-        v_root = find(v)
-        if u_root != v_root:
-            parent[v_root] = u_root
+for num in arr:
+    r = num % k
+    complement = (k - r) % k
+    count += freq[complement]
+    freq[r] += 1
 
-    for _ in range(m):
-        u, v, w = map(int, input().split())
-        
+print(count)
