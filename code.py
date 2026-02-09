00eed946e56a4a8b7c158032fb37ba2cd0138641
Analyzer <analyzer@test.com>
2026-02-09 13:51:08 -0800

    Update

--- code.py
+++ code.py
@@ -1,10 +1,14 @@
 n = int(input())
-grid = [list(map(int, input().split())) for _ in range(n)]
-row_sums = [sum(row) for row in grid]
-column_sums = [sum(col) for col in zip(*grid)]
-count = 0
-for i in range(n):
-    for j in range(n):
-        if column_sums[j] > row_sums[i]:
-            count += 1
-print(count)
+a = list(map(int, input().split()))
+from collections import defaultdict
+
+counts = defaultdict(int)
+for num in a:
+    bits = bin(num).count('1')
+    counts[bits] += 1
+
+result = 0
+for c in counts.values():
+    result += c * (c - 1) // 2
+
+print(result)
