1b0b5d3f9a0efd1d4835a585836ae1d4cb8afeda
Analyzer <analyzer@test.com>
2026-02-09 14:17:17 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,15 @@
-import collections
-
-def max_subsequence_sum(nums, k):
-    n = len(nums)
-    dp = [0] * n
-    dq = collections.deque()
-    max_sum = float('-inf')
+def findLongestWord(s, d):
+    def is_subsequence(word):
+        i = j = 0
+        n, m = len(s), len(word)
+        while i < n and j < m:
+            if s[i] == word[j]:
+                j += 1
+            i += 1
+        return j == m
     
-    for i in range(n):
-        # Remove indices out of the window [i-k, i-1]
-        while dq and dq[0] < i - k:
-            dq.popleft()
-        
-        # Calculate current dp value
-        prev_max = dp[dq[0]] if dq else 0
-        current = nums[i] + (prev_max if prev_max > 0 else 0)
-        dp[i] = current
-        
-        # Maintain deque in 
+    d_sorted = sorted(d, key=lambda x: (-len(x), x))
+    for word in d_sorted:
+        if is_subsequence(word):
+            return word
+    return ""
