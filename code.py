431f6b6f4dbf7c150303be5a9676a54b5ebde8c1
Analyzer <analyzer@test.com>
2026-02-09 13:04:20 -0800

    Update

--- code.py
+++ code.py
@@ -1,25 +1,8 @@
-import math
-
-a, b, c = map(int, input().split())
-
-if a == 0:
-    if b == 0:
-        if c == 0:
-            print(-1)
-        else:
-            print(0)
-    else:
-        x = -c / b
-        print(1)
-        print("{0:.10f}".format(x))
-else:
-    discriminant = b**2 - 4 * a * c
-    if discriminant < 0:
-        print(0)
-    elif discriminant == 0:
-        x = (-b) / (2 * a)
-        print(1)
-        print("{0:.10f}".format(x))
-    else:
-        sqrt_d = math.sqrt(discriminant)
-        x1 = (-b - sqrt
+a, b = map(int, input().split())
+total = a
+stubs = a
+while stubs >= b:
+    new = stubs // b
+    total += new
+    stubs = stubs % b + new
+print(total)
