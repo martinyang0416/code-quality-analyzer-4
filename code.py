3ee5bb43c9a12b113627dc226cad54dafb4f898f
Analyzer <analyzer@test.com>
2026-02-09 11:02:01 -0800

    Update

--- code.py
+++ code.py
@@ -1,5 +1,7 @@
-a = int(input())
-if a % 5 == 0 or a % 5 == 1:
-    print("YES")
-else:
-    print("NO")
+a = input().strip()
+count = 0
+for c in a:
+    digit = int(c)
+    if digit % 2 == 0:
+        count += 1
+print(0 if count >= 2 else 1)
