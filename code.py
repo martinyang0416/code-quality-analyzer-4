d7c4684eeee68dae4fa4f8f4657f4b6a8a08a21c
Analyzer <analyzer@test.com>
2026-02-09 14:09:32 -0800

    Update

--- code.py
+++ code.py
@@ -1,25 +1,19 @@
-from collections import defaultdict
-
-def main():
-    n = input().strip()
-    digits = list(n)
-    freq = defaultdict(int)
-    for d in digits:
-        freq[d] += 1
-
-    max_k = 0
-    best = None
-
-    for k in range(len(digits), 0, -1):
-        f = freq.copy()
-        pairs = []
-        possible = True
-
-        found = False
-        for d1 in '9876543210':
-            d2 = str(10 - int(d1))
-            if d1 > d2:
-                continue
-            if d1 == d2:
-                if f[d1] >= 2:
-  
+class FenwickTree:
+    def __init__(self, size):
+        self.n = size
+        self.tree_w = [0] * (self.n + 1)
+        self.tree_v = [0] * (self.n + 1)
+    
+    def update(self, idx, delta_w, delta_v):
+        idx += 1
+        while idx <= self.n:
+            self.tree_w[idx] += delta_w
+            self.tree_v[idx] += delta_v
+            idx += idx & -idx
+    
+    def query(self, idx):
+        idx += 1
+        sum_w = 0
+        sum_v = 0
+        while idx > 0:
+            sum_w += self.tree_w[i
